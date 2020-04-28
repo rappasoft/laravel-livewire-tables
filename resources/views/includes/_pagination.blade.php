6829d86bf2c8a5c1041cd2408e216b6d73f6348a
Anthony Rappa <rappa819@gmail.com>
2020-04-28 12:38:42 -0400

    Fix pagination text when table is empty.

--- resources/views/includes/_pagination.blade.php
+++ resources/views/includes/_pagination.blade.php
@@ -1,11 +1,11 @@
-@if ($paginationEnabled)
+@if ($paginationEnabled && $models->count())
     <div class="row">
         <div class="col">
             {{ $models->links() }}
         </div>
 
         <div class="col text-right text-muted">
-            {{ __('Showing :first to :last out of :total results', ['first' => $models->firstItem(), 'last' => $models->lastItem(), 'total' => $models->total()]) }}
+            {{ __('Showing :first to :last out of :total results', ['first' => $models->count() ? $models->firstItem() : 0, 'last' => $models->count() ? $models->lastItem() : 0, 'total' => $models->total()]) }}
         </div>
     </div>
 @endif
