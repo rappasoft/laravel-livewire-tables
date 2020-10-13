b2a553c4fea95ccb6b1b21d58c1cae561a2f51af
Carlos A. Guevara <carlos.guevara@gaiadesign.com.mx>
2020-10-13 09:22:12 -0500

    spanish strings

---
+++ resources/lang/es/strings.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'clear' => 'Limpiar',
+    'export' => 'Exportar',
+    'loading' => 'Cargando',
+    'no_results' => 'No se encontraron resultados para esta consulta.',
+    'offline' => 'Actualmente no te encuentras conectado a internet.',
+    'per_page' => 'Por pagina',
+    'results' => 'Mostrando :first a :last de :total resultados',
+    'search' => 'Buscar...',
+];
