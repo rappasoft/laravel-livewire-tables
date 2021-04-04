8a7c8ed5f923bb2c9a6e138cce652fac8b421b71
Dieter Coopman <dieter@deltasolutions.be>
2021-04-04 21:59:16 +0200

    remove sdfsdf
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- resources/views/tailwind/table-component.blade.php
+++ resources/views/tailwind/table-component.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col">sdfsdf
+<div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
