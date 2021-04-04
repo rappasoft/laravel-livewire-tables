9e8efc5d7a2d8908508b726fb58c2158d4868ffd
Dieter Coopman <dieter@deltasolutions.be>
2021-04-04 19:56:33 +0200

    fix translation error
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- resources/lang/nl/strings.php
+++ resources/lang/nl/strings.php
@@ -7,6 +7,6 @@ return [
     'no_results' => 'Er zijn geen resultaten voor deze zoekactie.',
     'offline' => 'U bent momenteel niet verbonden met het internet.',
     'per_page' => 'Aantal per pagina',
-    'results' => ':first tot :last resultaten geladen op een totaal van :total results',
+    'results' => ':first tot :last resultaten geladen op een totaal van :total resultaten',
     'search' => 'Zoek ...'
 ];
