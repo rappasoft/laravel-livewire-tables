064299f8dce9cb227783ab281979a821066111d2
bdelamatre <bdelamatre@users.noreply.github.com>
2021-04-20 02:05:14 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,11 +2,6 @@
 
 namespace Rappasoft\LaravelLivewireTables\Tests;
 
-use Illuminate\Container\Container;
-use Illuminate\Database\Capsule\Manager as DB;
-use Illuminate\Support\Facades\Facade;
-use Rappasoft\LaravelLivewireTables\Tests\Models\Species;
-
 class ExampleTest extends TestCase
 {
     /** @test */
