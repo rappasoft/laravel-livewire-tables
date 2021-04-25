ef78415efdaa3029f20f4d4598c8dbc0fbd32bf2
Byron DeLaMatre <byron@delamatre.com>
2021-04-25 16:33:12 -0400

    when pagination isn't in use or if there is only one page of results, adds count on tailwind theme

--- resources/views/tailwind/includes/pagination.blade.php
+++ resources/views/tailwind/includes/pagination.blade.php
@@ -1,5 +1,7 @@
-@if ($paginationEnabled && $showPerPage)
-    <div class="p-6 md:p-0">
+<div class="p-6 md:p-0">
+    @if ($paginationEnabled && $showPerPage && $rows->lastPage() > 1)
         {{ $rows->links() }}
-    </div>
-@endif
+    @else
+        <p class="text-sm text-gray-700 leading-5">Showing {{ $rows->count() }} results</p>
+    @endif
+</div>
