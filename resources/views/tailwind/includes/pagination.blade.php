4bbf22e3b5a3105c124e971d91507ad8113defe8
Byron DeLaMatre <byron@delamatre.com>
2021-05-14 14:14:17 -0400

    fixing tailwind pagination display options
    1) Eliminated the if block that wrapped the whole view because the view script should display its contents regardless of whether pagination is enabled or not. This is because it's also responsible for displaying the result count.
    
    2) Fixed the links() logic to check $showPagination and not $showPerPage. Show Per Page's effect should be isolated to the per page select element, not whether the pagination links should be displayed.

--- resources/views/tailwind/includes/pagination.blade.php
+++ resources/views/tailwind/includes/pagination.blade.php
@@ -1,13 +1,11 @@
-@if ($showPagination)
-    <div class="p-6 md:p-0">
-        @if ($paginationEnabled && $showPerPage && $rows->lastPage() > 1)
-            {{ $rows->links() }}
-        @else
-            <p class="text-sm text-gray-700 leading-5">
-                @lang('Showing')
-                <span class="font-medium">{{ $rows->count() }}</span>
-                @lang('results')
-            </p>
-        @endif
-    </div>
-@endif
+<div class="p-6 md:p-0">
+    @if ($paginationEnabled && $showPagination && $rows->lastPage() > 1)
+        {{ $rows->links() }}
+    @else
+        <p class="text-sm text-gray-700 leading-5">
+            @lang('Showing')
+            <span class="font-medium">{{ $rows->count() }}</span>
+            @lang('results')
+        </p>
+    @endif
+</div>
