fd8cff163e71ef44c7ff76f8d391938744db892f
Fabio Ivona <fabio.ivona@defstudio.it>
2021-05-17 23:22:45 +0200

    use column text for sorting and filter pills if no $filterNames exists
    Took 7 minutes

--- resources/views/bootstrap-4/includes/filter-pills.blade.php
+++ resources/views/bootstrap-4/includes/filter-pills.blade.php
@@ -8,7 +8,7 @@
                     wire:key="filter-pill-{{ $key }}"
                     class="badge badge-pill badge-info d-inline-flex align-items-center"
                 >
-                    {{ $filterNames[$key] ?? ucwords(strtr($key, ['_' => ' ', '-' => ' '])) }}:
+                    {{ $filterNames[$key] ?? collect($this->columns())->pluck('text', 'column')->get($key, ucwords(strtr($key, ['_' => ' ', '-' => ' ']))) }}:
                     @if(isset($customFilters[$key]) && method_exists($customFilters[$key], 'options'))
                         {{ $customFilters[$key]->options()[$value] ?? $value }}
                     @else
