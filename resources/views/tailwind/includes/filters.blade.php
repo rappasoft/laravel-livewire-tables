ad0d3117f966bb94cd1c930e5735c116856a1eb7
Stephen G <45797159+stephengalbraith@users.noreply.github.com>
2021-05-31 18:56:56 -0400

    Update filters.blade.php

--- resources/views/tailwind/includes/filters.blade.php
+++ resources/views/tailwind/includes/filters.blade.php
@@ -50,7 +50,7 @@
 
                                 <div class="mt-1 relative rounded-md shadow-sm">
                                     <select
-                                        wire:model.prevent="filters.{{ $key }}"
+                                        wire:model.stop="filters.{{ $key }}"
                                         wire:key="filter-{{ $key }}"
                                         id="filter-{{ $key }}"
                                         class="rounded-md shadow-sm block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo sm:text-sm sm:leading-5"
