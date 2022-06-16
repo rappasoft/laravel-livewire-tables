4c68848817b35596cb722774360733ec4f846af6
denis <denis.gillier@gmail.com>
2022-06-16 19:24:23 +0200

    Column style change

--- resources/views/components/table/th/plain.blade.php
+++ resources/views/components/table/th/plain.blade.php
@@ -5,7 +5,7 @@
 @endphp
 
 @if ($theme === 'tailwind')
-    <th scope="col" {{ $attributes->merge(['class' => 'table-cell px-3 py-2 md:px-6 md:py-3 text-center md:text-left bg-gray-50 dark:bg-gray-800']) }}>{{ $slot }}</th>
+    <th scope="col" {{ $attributes->merge(['class' => 'table-cell px-3 py-2 md:px-6 md:py-3 text-center md:text-left  dark:bg-gray-800']) }}>{{ $slot }}</th>
 @elseif ($theme === 'bootstrap-4' || $theme === 'bootstrap-5')
     <th scope="col">{{ $slot }}</th>
 @endif
