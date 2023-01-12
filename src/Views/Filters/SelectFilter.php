2abc85c82573c2d4e3d369e6a5219895be995051
Joe <joe@lowerrocklabs.com>
2023-01-12 18:27:01 +0000

    Adding option to set first option for select filter

--- src/Views/Filters/SelectFilter.php
+++ src/Views/Filters/SelectFilter.php
@@ -9,11 +9,25 @@ class SelectFilter extends Filter
 {
     protected array $options = [];
 
+    protected string $firstOption = "";
+
     public function options(array $options = []): SelectFilter
     {
         $this->options = $options;
 
         return $this;
+    }
+
+    public function setFirstOption(string $firstOption): SelectFilter
+    {
+        $this->firstOption = $firstOption;
+
+        return $this;
+    }
+
+    public function getFirstOption(): string
+    {
+        return $this->firstOption;
     }
 
     public function getOptions(): array
