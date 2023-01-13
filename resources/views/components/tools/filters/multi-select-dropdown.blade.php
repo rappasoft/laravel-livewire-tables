2e11536ce43748c1d0640b702992db199ae37947
Joe <joe@lowerrocklabs.com>
2023-01-13 06:54:29 +0000

    Fixes for language strins and replacing MultiSelectDropdownFilter with improved formatting

--- resources/views/components/tools/filters/multi-select-dropdown.blade.php
+++ resources/views/components/tools/filters/multi-select-dropdown.blade.php
@@ -4,11 +4,11 @@
 
 @if ($theme === 'tailwind')
     <div class="rounded-md shadow-sm">
-        <select
+        <select multiple
             wire:model.stop="{{ $component->getTableName() }}.filters.{{ $filter->getKey() }}"
             wire:key="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}"
             id="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}"
-            class="block w-full border-gray-300 rounded-md shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600"
+            class="block w-full transition duration-150 ease-in-out border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600"
         >
             @if ($filter->getFirstOption() != "")
             <option value="">{{ $filter->getFirstOption()}}</option>
@@ -27,15 +27,16 @@
         </select>
     </div>
 @elseif ($theme === 'bootstrap-4' || $theme === 'bootstrap-5')
-    <select
+    <select multiple
         wire:model.stop="{{ $component->getTableName() }}.filters.{{ $filter->getKey() }}"
         wire:key="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}"
         id="{{ $component->getTableName() }}-filter-{{ $filter->getKey() }}"
         class="{{ $theme === 'bootstrap-4' ? 'form-control' : 'form-select' }}"
     >
-    @if ($filter->getFirstOption() != "")
-    <option value="">{{ $filter->getFirstOption()}}</option>
-    @endif
+        @if ($filter->getFirstOption() != "")
+        <option value="">{{ $filter->getFirstOption()}}</option>
+        @endif
+
         @foreach($filter->getOptions() as $key => $value)
             @if (is_iterable($value))
                 <optgroup label="{{ $key }}">
