c075d620e6f51c132cd67814a430168863235352
lrljoe <104938042+lrljoe@users.noreply.github.com>
2023-02-17 20:05:15 +0000

    Update BulkActionsHelpers.php

--- src/Traits/Helpers/BulkActionsHelpers.php
+++ src/Traits/Helpers/BulkActionsHelpers.php
@@ -188,14 +188,14 @@ trait BulkActionsHelpers
         $this->setSelected((clone $this->baseQuery())->pluck($this->getPrimaryKey())->map(fn ($item) => (string)$item)->toArray());
     }
     
-    /**
+     /**
      * Get custom attributes for the Bulk Selections Table Column
      */
     public function getBulkSelectionsTdAttributes(): array
     {
         return $this->bulkSelectionsTdAttributes;
     }
-    
+
     /**
      * Get custom attributes for the Bulk Selections Checkbox Container
      */
