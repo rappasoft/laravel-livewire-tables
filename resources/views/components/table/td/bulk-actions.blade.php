67ea3b98a71ac552514f79375ec9bf3e6a1ad12e
lrljoe <104938042+lrljoe@users.noreply.github.com>
2023-02-17 19:34:28 +0000

    Update bulk-actions.blade.php

--- resources/views/components/table/td/bulk-actions.blade.php
+++ resources/views/components/table/td/bulk-actions.blade.php
@@ -4,38 +4,32 @@
 @if ($component->bulkActionsAreEnabled() && $component->hasBulkActions())
     @php
         $theme = $component->getTheme();
+        $customAttributesTD = $this->getBulkSelectionsTdAttributes();
+        $customAttributes['bulkcontainer'] = $this->getBulkSelectionsContainerAttributes();
+        $customAttributes['bulkinput'] = $this->getBulkSelectionsInputAttributes();
     @endphp
 
     @if ($theme === 'tailwind')
-        <x-livewire-tables::table.td.plain>
-            <div class="inline-flex rounded-md shadow-sm">
-                <input
-                    wire:model="selected"
-                    wire:loading.attr.delay="disabled"
-                    value="{{ $row->{$this->getPrimaryKey()} }}"
-                    type="checkbox"
-                    class="rounded border-gray-300 text-indigo-600 shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-900 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600 dark:focus:bg-gray-600"
-                />
+        <x-livewire-tables::table.td.plain :customAttributes=$customAttributesTD>
+            <div
+                {{ $attributes->merge($customAttributes['bulkcontainer'])->class(['inline-flex rounded-md shadow-sm' => $customAttributes['bulkcontainer']['default'] ?? true])->except('default') }}>
+                <input wire:model="selected" wire:loading.attr.delay="disabled"
+                    value="{{ $row->{$this->getPrimaryKey()} }}" type="checkbox"
+                    {{ $attributes->merge($customAttributes['bulkinput'])->class(['rounded border-gray-300 text-indigo-600 shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-900 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600 dark:focus:bg-gray-600' => $customAttributes['bulkinput']['default'] ?? true])->except('default') }} />
             </div>
         </x-livewire-tables::table.td.plain>
     @elseif ($theme === 'bootstrap-4')
-        <x-livewire-tables::table.td.plain>
-            <input
-                wire:model="selected"
-                wire:loading.attr.delay="disabled"
-                value="{{ $row->{$this->getPrimaryKey()} }}"
-                type="checkbox"
-            />
+        <x-livewire-tables::table.td.plain :customAttributes=$customAttributesTD>
+            <input wire:model="selected" wire:loading.attr.delay="disabled" value="{{ $row->{$this->getPrimaryKey()} }}"
+                type="checkbox" />
         </x-livewire-tables::table.td.plain>
     @elseif ($theme === 'bootstrap-5')
-        <x-livewire-tables::table.td.plain>
-            <div class="form-check">
-                <input
-                    wire:model="selected"
-                    wire:loading.attr.delay="disabled"
-                    value="{{ $row->{$this->getPrimaryKey()} }}"
-                    type="checkbox"
-                    class="form-check-input"
+        <x-livewire-tables::table.td.plain :customAttributes=$customAttributesTD>
+            <div
+                {{ $attributes->merge($customAttributes['bulkcontainer'])->class(['form-check' => $customAttributes['bulkcontainer']['default'] ?? true])->except('default') }}>
+                <input wire:model="selected" wire:loading.attr.delay="disabled"
+                    value="{{ $row->{$this->getPrimaryKey()} }}" type="checkbox"
+                    {{ $attributes->merge($customAttributes['bulkinput'])->class(['form-check-input' => $customAttributes['bulkinput']['default'] ?? true])->except('default') }} />
                 />
             </div>
         </x-livewire-tables::table.td.plain>
