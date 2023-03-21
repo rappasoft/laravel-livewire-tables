6ef101d4f72503718f5cfee4fcad0fc2605ac63f
lrljoe <joe@lowerrocklabs.com>
2023-03-21 00:34:40 +0000

    Initial Commits

--- resources/views/components/wrapper.blade.php
+++ resources/views/components/wrapper.blade.php
@@ -6,6 +6,38 @@
 @endphp
 
  <div
+        x-data="{ 
+            @if ($component->isFilterLayoutSlideDown()) filtersOpen: $wire.filterSlideDownDefaultVisible, @endif
+            @if ($component->bulkActionsAreEnabled())
+            selectedItems: $wire.entangle('selected'),
+            visibleItems: {},
+            selectAllOnPage() {
+                let tempSelectedItems = this.selectedItems;
+                
+                const iterator = visibleItems.values();
+                for (const value of iterator) {
+                    tempSelectedItems.push(value.toString());
+                }
+                this.selectedItems = [...new Set(tempSelectedItems)];
+            },
+            toggleSelectedItem(itemID) {
+                let selectedItemIndex = this.selectedItems.indexOf(itemID);
+
+                if (selectedItemIndex !== undefined && selectedItemIndex !== -1)
+                {
+                    let tempSelectedItems = this.selectedItems;
+                    let itemIndex = tempSelectedItems.indexOf(itemID);
+                    while (itemIndex !== -1) {
+                        tempSelectedItems.splice(itemIndex, 1);
+                        itemIndex = tempSelectedItems.indexOf(itemID);
+                    }
+                    this.selectedItems = [...new Set(tempSelectedItems)];
+                } else {
+                    this.selectedItems.push(itemID);
+                }
+            },
+            @endif
+        }"
     {{ $attributes->merge($this->getComponentWrapperAttributes()) }}
 
     @if ($component->hasRefresh())
@@ -13,7 +45,7 @@
     @endif
 
     @if ($component->isFilterLayoutSlideDown())
-        wire:ignore.self x-data="{ filtersOpen: $wire.filterSlideDownDefaultVisible }"
+        wire:ignore.self 
     @endif
 >
      @include('livewire-tables::includes.debug')
