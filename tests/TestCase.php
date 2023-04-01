063518adae3e9e12669eeb7fe5e1fe086089323c
lrljoe <joe@lowerrocklabs.com>
2023-04-01 21:42:48 +0100

    Fixes for DB Creation

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,15 @@
 namespace Rappasoft\LaravelLivewireTables\Tests;
 
 use Illuminate\Encryption\Encrypter;
+use Illuminate\Support\Facades\DB;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Rappasoft\LaravelLivewireTables\LaravelLivewireTablesServiceProvider;
 use Rappasoft\LaravelLivewireTables\Tests\Http\Livewire\PetsTable;
+use Rappasoft\LaravelLivewireTables\Tests\Models\Breed;
+use Rappasoft\LaravelLivewireTables\Tests\Models\Pet;
+use Rappasoft\LaravelLivewireTables\Tests\Models\Species;
+use Rappasoft\LaravelLivewireTables\Tests\Models\Veterinary;
 
 class TestCase extends Orchestra
 {
