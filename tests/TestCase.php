036cb06ea2c72c32a5d6bada81c720382886a867
lrljoe <lrljoe@users.noreply.github.com>
2023-04-01 14:24:49 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -28,8 +28,7 @@ class TestCase extends Orchestra
         $this->basicTable->boot();
         $this->basicTable->booted();
         $this->basicTable->render();
-        if (Species::where('id', 1)->count() != 1)
-        {
+        if (Species::where('id', 1)->count() != 1) {
             Species::insert([
                 ['id' => 1, 'name' => 'Cat'],
                 ['id' => 2, 'name' => 'Dog'],
