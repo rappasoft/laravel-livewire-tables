a7bd72afe7d47a33f65d712dedd5185638cbdb24
lrljoe <joe@lowerrocklabs.com>
2023-04-01 08:43:17 +0100

    Tests

--- tests/Views/Filters/DateFilterTest.php
+++ tests/Views/Filters/DateFilterTest.php
@@ -9,7 +9,7 @@ final class DateFilterTest extends FilterTestCase
 
     public function setUp(): void
     {
-        $this->filterInstance = DateFilter::make('Created Date');
+        self::$filterInstance = DateFilter::make('Created Date');
     }
 
     public function testCreateFilterInstance()
@@ -18,61 +18,57 @@ final class DateFilterTest extends FilterTestCase
     }
 
     /**
-     * @depends testCreateFilterInstance
+     * @test
      */
-    public function test_can_not_set_date_filter_to_non_number($filterInstance): void
+    public function test_can_not_set_date_filter_to_non_number(): void
     {
-        $this->assertFalse($filterInstance->validate('test'));
+        $this->assertFalse(self::$filterInstance->validate('test'));
     }
 
     /**
      * @test
-     * @depends testCreateFilterInstance
      */
-    public function test_can_not_set_date_filter_to_number($filterInstance): void
+    public function test_can_not_set_date_filter_to_number(): void
     {
-        $this->assertFalse($filterInstance->validate(123));
-        $this->assertFalse($filterInstance->validate('123'));
+        $this->assertFalse(self::$filterInstance->validate(123));
+        $this->assertFalse(self::filterInstance->validate('123'));
     }
 
     /**
      * @test
-     * @depends testCreateFilterInstance
      */
-    public function test_can_not_set_date_filter_to_invalid_date($filterInstance): void
+    public function test_can_not_set_date_filter_to_invalid_date(): void
     {
-        $this->assertFalse($filterInstance->validate('123'));
-        $this->assertFalse($filterInstance->validate('Test'));
-        $this->assertFalse($filterInstance->validate('12/01/2001'));
-        $this->assertFalse($filterInstance->validate('12/01/201'));
-        $this->assertFalse($filterInstance->validate('12-01-201'));
-        $this->assertFalse($filterInstance->validate('12-01-2014'));
-        $this->assertFalse($filterInstance->validate('2014/01/01'));
-        $this->assertSame('2020-01-01', $filterInstance->validate('2020-01-01'));
+        $this->assertFalse(self::$filterInstance->validate('123'));
+        $this->assertFalse(self::$filterInstance->validate('Test'));
+        $this->assertFalse(self::$filterInstance->validate('12/01/2001'));
+        $this->assertFalse(self::$filterInstance->validate('12/01/201'));
+        $this->assertFalse(self::$filterInstance->validate('12-01-201'));
+        $this->assertFalse(self::$filterInstance->validate('12-01-2014'));
+        $this->assertFalse(self::$filterInstance->validate('2014/01/01'));
+        $this->assertSame('2020-01-01', self::$filterInstance->validate('2020-01-01'));
     }
 
     /**
      * @test
-     * @depends testCreateFilterInstance
      */    
-    public function test_can_get_if_date_filter_empty($filterInstance): void
+    public function test_can_get_if_date_filter_empty(): void
     {
-        $this->assertTrue($filterInstance->isEmpty(''));
+        $this->assertTrue(self::$filterInstance->isEmpty(''));
     }
 
     /**
      * @test
-     * @depends testCreateFilterInstance
      */    
-    public function test_can_not_set_date_filter_to_invalid_date_custom_format($filterInstance): void
+    public function test_can_not_set_date_filter_to_invalid_date_custom_format(): void
     {
-        $this->assertFalse($filterInstance->validate('123'));
-        $this->assertFalse($filterInstance->validate('Test'));
-        $this->assertFalse($filterInstance->validate('12/01/2001'));
-        $this->assertFalse(filterInstance->validate('12/01/201'));
-        $this->assertFalse($filterInstance->validate('12-01-201'));
-        $this->assertFalse(filterInstance->validate('12-01-2014'));
-        $this->assertFalse($filterInstance->validate('2014/01/01'));
-        $this->assertSame('2020-01-01', $filterInstance->validate('2020-01-01'));
+        $this->assertFalse(self::$filterInstance->validate('123'));
+        $this->assertFalse(self::$filterInstance->validate('Test'));
+        $this->assertFalse(self::$filterInstance->validate('12/01/2001'));
+        $this->assertFalse(self::$filterInstance->validate('12/01/201'));
+        $this->assertFalse(self::$filterInstance->validate('12-01-201'));
+        $this->assertFalse(self::$filterInstance->validate('12-01-2014'));
+        $this->assertFalse(self::$filterInstance->validate('2014/01/01'));
+        $this->assertSame('2020-01-01', self::$filterInstance->validate('2020-01-01'));
     }
 }
