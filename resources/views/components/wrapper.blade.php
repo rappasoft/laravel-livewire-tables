aec980b2bf99b0d23a83eb29b796c74c0576a06c
lrljoe <joe@lowerrocklabs.com>
2023-04-28 00:12:31 +0100

    Wrapper Fixes - Add SelectedItems Elements

--- resources/views/components/wrapper.blade.php
+++ resources/views/components/wrapper.blade.php
@@ -5,35 +5,29 @@
     $theme = $component->getTheme();
 @endphp
 
- <div x-data="{ 
-            @if ($component->isFilterLayoutSlideDown()) filtersOpen: $wire.filterSlideDownDefaultVisible, @endif
-            @if ($component->bulkActionsAreEnabled())
-            selectedItems: $wire.entangle('selected').defer,
-            visibleItems: {},
-            selectAllOnPage() {
-                let tempSelectedItems = this.selectedItems;
-                const iterator = visibleItems.values();
-                for (const value of iterator) {
-                    tempSelectedItems.push(value.toString());
-                }
-                this.selectedItems = [...new Set(tempSelectedItems)];
-            },
-            @endif
-        }">
-    <div
-        {{ $attributes->merge($this->getComponentWrapperAttributes()) }}
+<div x-data="{
+    shouldShowBulkActionSelect: false,
+    @if ($component->isFilterLayoutSlideDown()) filtersOpen: $wire.filterSlideDownDefaultVisible, @endif
+    @if ($component->bulkActionsAreEnabled()) selectedItems: $wire.entangle('selected').defer,
+        selectedCount: 0,
+        visibleItems: {},
+        allItemsSelected: false,
+        selectAllOnPage() {
+            let tempSelectedItems = this.selectedItems;
+            const iterator = visibleItems.values();
+            for (const value of iterator) {
+                tempSelectedItems.push(value.toString());
+            }
+            this.selectedItems = [...new Set(tempSelectedItems)];
+        }, @endif
+}"
+    x-effect="shouldShowBulkActionSelect = (selectedItems.length > 0); selectedCount = selectedItems.length;">
+    <div {{ $attributes->merge($this->getComponentWrapperAttributes()) }}
+        @if ($component->hasRefresh()) wire:poll{{ $component->getRefreshOptions() }} @endif
+        @if ($component->isFilterLayoutSlideDown()) wire:ignore.self @endif>
+        @include('livewire-tables::includes.debug')
+        @include('livewire-tables::includes.offline')
 
-        @if ($component->hasRefresh())
-            wire:poll{{ $component->getRefreshOptions() }}
-        @endif
-
-        @if ($component->isFilterLayoutSlideDown())
-            wire:ignore.self 
-        @endif
-    >
-         @include('livewire-tables::includes.debug')
-         @include('livewire-tables::includes.offline')
-
-         {{ $slot }}
+        {{ $slot }}
     </div>
 </div>
