b4ed671f8081eb40bfbcc67a45b4f20a8549748c
lrljoe <joe@lowerrocklabs.com>
2023-05-18 22:03:52 +0100

    Theme to Conditional Class in Bulk Actions th & td

--- resources/views/components/table/th/bulk-actions.blade.php
+++ resources/views/components/table/th/bulk-actions.blade.php
@@ -5,35 +5,24 @@
         $theme = $component->getTheme();
     @endphp
 
-    @if ($theme === 'tailwind')
-        <x-livewire-tables::table.th.plain>
-            <div class="inline-flex rounded-md shadow-sm" >
-                <input
-                    :checked="selectedItems.length == totalItemCount"
-                    x-on:click="toggleSelectAll"
-                    type="checkbox"
-                    class="rounded border-gray-300 text-indigo-600 shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-900 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600 dark:focus:bg-gray-600"
-                />
-            </div>
-        </x-livewire-tables::table.th.plain>
-    @elseif ($theme === 'bootstrap-4')
-        <x-livewire-tables::table.th.plain>
-            <input
-                :checked="selectedItems.length == totalItemCount"
+    <x-livewire-tables::table.th.plain>
+        <div
+            x-data="{ bulkActionHeaderChecked: false}"  
+            x-init="$watch('selectedItems', value => bulkActionHeaderChecked = (value.length === totalItemCount))"
+            @class([
+                'inline-flex rounded-md shadow-sm' => $theme === 'tailwind',
+                'form-check' => $theme === 'bootstrap-5',
+                ])
+        >
+            <input 
+                x-model="bulkActionHeaderChecked"
                 x-on:click="toggleSelectAll"
                 type="checkbox"
+                @class([
+                    'rounded border-gray-300 text-indigo-600 shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-900 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600 dark:focus:bg-gray-600' => $theme === 'tailwind',
+                    'form-check-input' => $theme === 'bootstrap-5',
+                ])
             />
-        </x-livewire-tables::table.th.plain>
-    @elseif ($theme === 'bootstrap-5')
-        <x-livewire-tables::table.th.plain>
-            <div class="form-check">
-                <input
-                    :checked="selectedItems.length == totalItemCount"
-                    x-on:click="toggleSelectAll"
-                    type="checkbox"
-                    class="form-check-input"
-                />
-            </div>
-        </x-livewire-tables::table.th.plain>
-    @endif
+        </div>
+    </x-livewire-tables::table.th.plain>
 @endif
