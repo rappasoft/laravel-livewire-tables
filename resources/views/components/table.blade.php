44ac99a6668902906662be6f071167f04e6ee33e
LRLJoe <joe@lowerrocklabs.com>
2023-08-01 08:42:24 +0000

    Add IDs to datatable elements

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -10,26 +10,26 @@
 @endphp
 
 @if ($component->isTailwind())
-    <div {{
+    <div wire:key="{{ $tableName }}-twrap" id="{{ $customAttributes['wrapper']['id'] ?? $tableName.'-twrap' }}" {{
         $attributes->merge($customAttributes['wrapper'])
             ->class(['shadow overflow-y-scroll border-b border-gray-200 dark:border-gray-700 sm:rounded-lg' => $customAttributes['wrapper']['default'] ?? true])
             ->except('default')
     }}>
-        <table wire:key="{{ $tableName }}-table" {{
+        <table wire:key="{{ $tableName }}-table" id="{{ $customAttributes['table']['id'] ?? $tableName.'-table' }}" {{
             $attributes->merge($customAttributes['table'])
                 ->class(['min-w-full divide-y divide-gray-200 dark:divide-none' => $customAttributes['table']['default'] ?? true])
                 ->except('default')
         }}>
-            <thead {{
+            <thead wire:key="{{ $tableName }}-thead" id="{{ $customAttributes['thead']['id'] ?? $tableName.'-thead' }}" {{
                 $attributes->merge($customAttributes['thead'])
                     ->class(['bg-gray-50' => $customAttributes['thead']['default'] ?? true])
                     ->except('default')
             }}>
-                <tr>
+                <tr wire:key="{{ $tableName }}-thead-row">
                     {{ $thead }}
                 </tr>
             </thead>
-            <tbody id="{{ $tableName }}-tbody"
+            <tbody wire:key="{{ $tableName }}-tbody" id="{{ $customAttributes['tbody']['id'] ?? $tableName.'-tbody' }}"
                 {{
                     $attributes->merge($customAttributes['tbody'])
                         ->class(['bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-none' => $customAttributes['tbody']['default'] ?? true])
@@ -40,34 +40,34 @@
             </tbody>
 
             @if (isset($tfoot))
-                <tfoot>
+                <tfoot wire:key="{{ $tableName }}-tfoot">
                     {{ $tfoot }}
                 </tfoot>
             @endif
         </table>
     </div>
 @elseif ($component->isBootstrap())
-    <div {{
+    <div wire:key="{{ $tableName }}-twrap"  id="{{ $customAttributes['wrapper']['id'] ?? $tableName.'-twrap' }}" {{
         $attributes->merge($customAttributes['wrapper'])
             ->class(['table-responsive' => $customAttributes['wrapper']['default'] ?? true])
             ->except('default')
     }}>
-        <table wire:key="{{ $tableName }}-table" {{
+        <table wire:key="{{ $tableName }}-table" id="{{ $customAttributes['table']['id'] ?? $tableName.'-table' }}"  {{
             $attributes->merge($customAttributes['table'])
                 ->class(['laravel-livewire-table table' => $customAttributes['table']['default'] ?? true])
                 ->except('default')
         }}>
-            <thead {{
+            <thead  wire:key="{{ $tableName }}-thead" id="{{ $customAttributes['thead']['id'] ?? $tableName.'-thead' }}" {{
                 $attributes->merge($customAttributes['thead'])
                     ->class(['' => $customAttributes['thead']['default'] ?? true])
                     ->except('default')
             }}>
-                <tr>
+                <tr wire:key="{{ $tableName }}-thead-row">
                     {{ $thead }}
                 </tr>
             </thead>
 
-            <tbody id="{{ $tableName }}-tbody"
+            <tbody wire:key="{{ $tableName }}-tbody" id="{{ $customAttributes['tbody']['id'] ?? $tableName.'-tbody' }}"
                 {{
                     $attributes->merge($customAttributes['tbody'])
                         ->class(['' => $customAttributes['tbody']['default'] ?? true])
@@ -78,7 +78,7 @@
             </tbody>
 
             @if (isset($tfoot))
-                <tfoot>
+                <tfoot wire:key="{{ $tableName }}-tfoot">
                     {{ $tfoot }}
                 </tfoot>
             @endif
