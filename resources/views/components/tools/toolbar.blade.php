d302637ab98329bb648a467665519c047921e720
LRLJoe <joe@lowerrocklabs.com>
2023-08-01 03:12:03 +0000

    Add fix for standard style

--- resources/views/components/tools/toolbar.blade.php
+++ resources/views/components/tools/toolbar.blade.php
@@ -18,7 +18,7 @@
             </div>
             <div x-show="reorderStatus">
                 <button
-                    x-on:click="reorderToggle()"
+                    x-on:click="reorderToggle"
                     type="button"
                     class="inline-flex justify-center items-center w-full md:w-auto px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600">
                     <span x-show="reorderCurrentStatus">
