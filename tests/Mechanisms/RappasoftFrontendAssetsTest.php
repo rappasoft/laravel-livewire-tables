e902debf3f1fbd7dae30af52077a5cbb2ede439a
lrljoe <lrljoe@users.noreply.github.com>
2023-09-03 21:07:23 +0000

    Fix styling

--- tests/Mechanisms/RappasoftFrontendAssetsTest.php
+++ tests/Mechanisms/RappasoftFrontendAssetsTest.php
@@ -2,35 +2,32 @@
 
 namespace Rappasoft\LaravelLivewireTables\Tests\Mechanisms;
 
-use Rappasoft\LaravelLivewireTables\Tests\TestCase;
 use Rappasoft\LaravelLivewireTables\Mechanisms\RappasoftFrontendAssets;
-use function Livewire\trigger;
+use Rappasoft\LaravelLivewireTables\Tests\TestCase;
 
 class RappasoftFrontendAssetsTest extends TestCase
 {
+    /** @test */
+    public function styles()
+    {
+        $assets = app(RappasoftFrontendAssets::class);
 
-        /** @test */
-        public function styles()
-        {
-            $assets = app(RappasoftFrontendAssets::class);
-    
-            $this->assertFalse($assets->hasRenderedRappsoftTableStyles);
-    
-            $this->assertStringStartsWith('<link href="/rappasoft/laravel-livewire-tables/core.min.css" rel="stylesheet" />', ltrim($assets->tableStyles()));
-    
-            $this->assertTrue($assets->hasRenderedRappsoftTableStyles);
-        }
-    
-        /** @test */
-        public function scripts()
-        {
-            $assets = app(RappasoftFrontendAssets::class);
-    
-            $this->assertFalse($assets->hasRenderedRappsoftTableScripts);
-    
-            $this->assertStringStartsWith('<script src="', $assets->tableScripts());
-    
-            $this->assertTrue($assets->hasRenderedRappsoftTableScripts);
-        }
+        $this->assertFalse($assets->hasRenderedRappsoftTableStyles);
 
+        $this->assertStringStartsWith('<link href="/rappasoft/laravel-livewire-tables/core.min.css" rel="stylesheet" />', ltrim($assets->tableStyles()));
+
+        $this->assertTrue($assets->hasRenderedRappsoftTableStyles);
+    }
+
+    /** @test */
+    public function scripts()
+    {
+        $assets = app(RappasoftFrontendAssets::class);
+
+        $this->assertFalse($assets->hasRenderedRappsoftTableScripts);
+
+        $this->assertStringStartsWith('<script src="', $assets->tableScripts());
+
+        $this->assertTrue($assets->hasRenderedRappsoftTableScripts);
+    }
 }
