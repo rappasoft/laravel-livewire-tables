b200fde539859b8c364028fa88f7c7b45d8ea03d
lrljoe <lrljoe@users.noreply.github.com>
2023-09-26 04:59:57 +0000

    Fix styling

--- tests/DataTransferObjects/DebuggableDataTest.php
+++ tests/DataTransferObjects/DebuggableDataTest.php
@@ -2,8 +2,8 @@
 
 namespace Rappasoft\LaravelLivewireTables\Tests\DataTransferObjects;
 
-use Rappasoft\LaravelLivewireTables\Tests\TestCase;
 use Rappasoft\LaravelLivewireTables\DataTransferObjects\DebuggableData;
+use Rappasoft\LaravelLivewireTables\Tests\TestCase;
 
 class DebuggableDataTest extends TestCase
 {
