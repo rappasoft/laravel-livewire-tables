5b248759d2400a2603dc89ab1c2829f819762a48
Joe <104938042+lrljoe@users.noreply.github.com>
2023-09-26 01:50:36 +0100

    Test Fixes - Config Variable Changes

--- tests/Features/AutoInjectRappasoftAssetsTest.php
+++ tests/Features/AutoInjectRappasoftAssetsTest.php
@@ -10,30 +10,26 @@ class AutoInjectRappasoftAssetsTest extends TestCase
     /** @test */
     public function shouldInjectRappasoftAndThirdParty()
     {
-        config()->set('livewire-tables.inject_assets', true);
-        config()->set('livewire-tables.inject_third_party_assets', true);
-        config()->set('livewire-tables.published_third_party_assets', false);
-        config()->set('livewire-tables.remote_third_party_assets', false);
+        config()->set('livewire-tables.inject_core_assets_enabled', true);
+        config()->set('livewire-tables.inject_third_party_assets_enabled', true);
+
         $this->assertEquals('<html><head>    <link href="/rappasoft/laravel-livewire-tables/core.min.css" rel="stylesheet" />     <link href="/rappasoft/laravel-livewire-tables/thirdparty.css" rel="stylesheet" /><script src="/rappasoft/laravel-livewire-tables/core.min.js"   ></script> <script src="/rappasoft/laravel-livewire-tables/thirdparty.min.js"  type="module"  ></script></head><body></body></html>', AutoInjectRappasoftAssets::injectAssets('<html><head></head><body></body></html>'));
     }
 
     /** @test */
     public function shouldNotInjectRappasoftOrThirdParty()
     {
-        config()->set('livewire-tables.inject_assets', false);
-        config()->set('livewire-tables.inject_third_party_assets', false);
-        config()->set('livewire-tables.published_third_party_assets', false);
-        config()->set('livewire-tables.remote_third_party_assets', false);
+        config()->set('livewire-tables.inject_core_assets_enabled', false);
+        config()->set('livewire-tables.inject_third_party_assets_enabled', false);
+
         $this->assertEquals('<html><head>  </head><body></body></html>', AutoInjectRappasoftAssets::injectAssets('<html><head></head><body></body></html>'));
     }
 
     /** @test */
     public function shouldOnlyInjectThirdParty()
     {
-        config()->set('livewire-tables.inject_assets', false);
-        config()->set('livewire-tables.inject_third_party_assets', true);
-        config()->set('livewire-tables.published_third_party_assets', false);
-        config()->set('livewire-tables.remote_third_party_assets', false);
+        config()->set('livewire-tables.inject_core_assets_enabled', false);
+        config()->set('livewire-tables.inject_third_party_assets_enabled', true);
 
         $this->assertEquals('<html><head>     <link href="/rappasoft/laravel-livewire-tables/thirdparty.css" rel="stylesheet" /> <script src="/rappasoft/laravel-livewire-tables/thirdparty.min.js"  type="module"  ></script></head><body></body></html>', AutoInjectRappasoftAssets::injectAssets('<html><head></head><body></body></html>'));
     }
@@ -41,10 +37,8 @@ class AutoInjectRappasoftAssetsTest extends TestCase
     /** @test */
     public function shouldOnlyInjectRappasoft()
     {
-        config()->set('livewire-tables.inject_assets', true);
-        config()->set('livewire-tables.inject_third_party_assets', false);
-        config()->set('livewire-tables.published_third_party_assets', false);
-        config()->set('livewire-tables.remote_third_party_assets', false);
+        config()->set('livewire-tables.inject_core_assets_enabled', true);
+        config()->set('livewire-tables.inject_third_party_assets_enabled', false);
 
         $this->assertEquals('<html><head>    <link href="/rappasoft/laravel-livewire-tables/core.min.css" rel="stylesheet" /> <script src="/rappasoft/laravel-livewire-tables/core.min.js"   ></script> </head><body></body></html>', AutoInjectRappasoftAssets::injectAssets('<html><head></head><body></body></html>'));
     }
