a93f3183ebc03436a8594a1b0c1862260e34df34
Joe <104938042+lrljoe@users.noreply.github.com>
2023-10-14 23:43:08 +0100

    Update Table Loader

--- resources/views/components/includes/loading.blade.php
+++ resources/views/components/includes/loading.blade.php
@@ -1,6 +1,6 @@
 @props(['colCount' => 1])
 
-<tr class="w-full text-center hidden h-screen place-items-center align-middle" wire:loading.class.remove="hidden">
+<tr class="w-full text-center hidden h-full place-items-center align-middle" wire:loading.class.remove="hidden">
     <td  class=" place-items-center	" colspan="{{ $colCount }}">
         <div class="h-min self-center align-middle">
             <div class="lds-hourglass"></div>
