ceee48d8e58bbf70518d1e3bb8e164c671e709d4
Joe <104938042+lrljoe@users.noreply.github.com>
2023-10-14 23:45:46 +0100

    Update Loading

--- resources/views/components/includes/loading.blade.php
+++ resources/views/components/includes/loading.blade.php
@@ -3,7 +3,7 @@
 <tr class="w-full text-center hidden h-full place-items-center align-middle" wire:loading.class.remove="hidden">
     <td  class=" place-items-center	" colspan="{{ $colCount }}">
         <div class="h-min self-center align-middle">
-            <div class="lds-hourglass"></div>
+            <div class="table-loader-hourglass"></div>
             <div>Loading...</div>
         </div>
     </td>
