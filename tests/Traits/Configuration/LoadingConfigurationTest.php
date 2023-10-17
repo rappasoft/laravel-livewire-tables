a22c6689a32ca18867cee098b4dd4d3b29e0f1f9
Joe <104938042+lrljoe@users.noreply.github.com>
2023-10-17 18:17:41 +0100

    Add Basic Tests for Loading Placeholder

---
+++ tests/Traits/Configuration/LoadingConfigurationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Tests\Traits\Configuration;
+
+use Rappasoft\LaravelLivewireTables\Tests\TestCase;
+
+class LoadingConfigurationTest extends TestCase
+{
+    /** @test */
+    public function can_set_loading_placeholder_status_enabled(): void
+    {
+        $this->assertFalse($this->basicTable->hasDisplayLoadingPlaceholder());
+
+        $this->basicTable->setLoadingPlaceholderEnabled();
+
+        $this->assertTrue($this->basicTable->hasDisplayLoadingPlaceholder());
+
+    }
+
+    /** @test */
+    public function can_set_loading_placeholder_status_disabled(): void
+    {
+        $this->assertFalse($this->basicTable->hasDisplayLoadingPlaceholder());
+
+        $this->basicTable->setLoadingPlaceholderEnabled();
+
+        $this->assertTrue($this->basicTable->hasDisplayLoadingPlaceholder());
+
+        $this->basicTable->setLoadingPlaceholderDisabled();
+
+        $this->assertFalse($this->basicTable->hasDisplayLoadingPlaceholder());
+
+    }
+
+}
---
+++ tests/Traits/Helpers/LoadingHelpersTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Tests\Traits\Helpers;
+
+use Rappasoft\LaravelLivewireTables\Tests\TestCase;
+
+class LoadingHelpersTest extends TestCase
+{
+    /** @test */
+    public function can_get_loading_placeholder_status(): void
+    {
+        $this->assertFalse($this->basicTable->hasDisplayLoadingPlaceholder());
+
+        $this->basicTable->setLoadingPlaceholderEnabled();
+
+        $this->assertTrue($this->basicTable->getDisplayLoadingPlaceholder());
+        
+        $this->assertTrue($this->basicTable->hasDisplayLoadingPlaceholder());
+
+    }
+
+
+}
