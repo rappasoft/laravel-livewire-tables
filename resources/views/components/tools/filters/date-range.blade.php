0cc1dea7818d6b5693366ce85cdb83ffa62492d2
Joe <104938042+lrljoe@users.noreply.github.com>
2023-10-29 00:48:06 +0100

    V3 DateRangeFilter - Remove Icon, Add Placeholder (#1492)
    * Remove DateRangeFilter Icon
    
    * DateRangeFilter - Remove Icon & Add Placeholder

--- resources/views/components/tools/filters/date-range.blade.php
+++ resources/views/components/tools/filters/date-range.blade.php
@@ -1,7 +1,5 @@
 @php
     $filterKey = $filter->getKey();
-    $filterConfigs = $filter->getConfigs();
-    $dateString = $filter->getDateString(isset($this->appliedFilters[$filterKey]) ? $this->appliedFilters[$filterKey] : '');  
 @endphp
 
 <div x-cloak id="{{ $tableName }}-dateRangeFilter-{{ $filterKey }}" x-data="flatpickrFilter($wire, '{{ $filterKey }}', @js($filter->getConfigs()), $refs.dateRangeInput, '{{ App::currentLocale() }}')" >
@@ -17,22 +15,15 @@
                 type="text"
                 x-ref="dateRangeInput"
                 x-on:click="init"
-                value="{{ $dateString }}"
+                value="{{ $filter->getDateString(isset($this->appliedFilters[$filterKey]) ? $this->appliedFilters[$filterKey] : '') }}"
                 wire:key="{{ $filter->generateWireKey($tableName, 'dateRange') }}"
                 id="{{ $tableName }}-filter-dateRange-{{ $filterKey }}"
                 @class([
-                    'w-10/12 inline-block align-middle transition duration-150 ease-in-out border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600' => $isTailwind,
-                    'd-inline-block form-control transition duration-150 ease-in-out border border-gray rounded-sm shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600' => $isBootstrap,
+                    'w-full inline-block align-middle transition duration-150 ease-in-out border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600' => $isTailwind,
+                    'd-inline-block w-100 form-control transition duration-150 ease-in-out border border-gray rounded-sm shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600' => $isBootstrap,
                 ])
-                @style([
-                    'width: 80%;' => $isBootstrap,
-                ])
-            />
-            @if($isTailwind)
-                <x-heroicon-o-calendar-days class="inline-block w-6 h-6 align-middle text-black dark:text-white group-hover:opacity-0"  />
-            @else
-                <x-heroicon-o-calendar-days class="d-inline-block" style="width:1em; height:1em;" />
-            @endif            
+                @if($filter->hasConfig('placeholder')) placeholder="{{ $filter->getConfig('placeholder') }}" @endif
+            />     
         </div>
     </div>
 </div>
