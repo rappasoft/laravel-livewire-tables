7bd7688f402e43e5e9096111833eeb169569eb6a
Joe <104938042+lrljoe@users.noreply.github.com>
2023-10-29 01:27:54 +0100

    Clean up classes on Filters

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ All notable changes to `laravel-livewire-tables` will be documented in this file
 - Add Filter CustomPosition tests
 - Add Placeholder config option for DateRangeFilter
 - Add Placeholder config option for DateFilter, DateTimeFilter, NumberFilter
+- Clean up classes on filters
 
 ## [3.0.0-beta.10] - 2023-10-27
 - Changes to toolbar blade structure by @lrljoe in #[1454](https://github.com/rappasoft/laravel-livewire-tables/pull/1454) 
--- resources/views/components/tools/filters/date-range.blade.php
+++ resources/views/components/tools/filters/date-range.blade.php
@@ -3,27 +3,25 @@
 @endphp
 
 <div x-cloak id="{{ $tableName }}-dateRangeFilter-{{ $filterKey }}" x-data="flatpickrFilter($wire, '{{ $filterKey }}', @js($filter->getConfigs()), $refs.dateRangeInput, '{{ App::currentLocale() }}')" >
-    <div>
-        <x-livewire-tables::tools.filter-label :$filter :$filterLayout :$tableName :$isTailwind :$isBootstrap4 :$isBootstrap5 :$isBootstrap />
-        <div
+    <x-livewire-tables::tools.filter-label :$filter :$filterLayout :$tableName :$isTailwind :$isBootstrap4 :$isBootstrap5 :$isBootstrap />
+    <div
+        @class([
+            'w-full rounded-md shadow-sm text-left ' => $isTailwind,
+            'd-inline-block w-100 mb-3 mb-md-0 input-group' => $isBootstrap,
+        ])
+    >
+        <input
+            type="text"
+            x-ref="dateRangeInput"
+            x-on:click="init"
+            value="{{ $filter->getDateString(isset($this->appliedFilters[$filterKey]) ? $this->appliedFilters[$filterKey] : '') }}"
+            wire:key="{{ $filter->generateWireKey($tableName, 'dateRange') }}"
+            id="{{ $tableName }}-filter-dateRange-{{ $filterKey }}"
             @class([
-                'w-full rounded-md shadow-sm text-left ' => $isTailwind,
-                'd-inline-block w-100 mb-3 mb-md-0 input-group' => $isBootstrap,
+                'w-full inline-block align-middle transition duration-150 ease-in-out border-gray-300 rounded-md shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600' => $isTailwind,
+                'd-inline-block w-100 form-control' => $isBootstrap,
             ])
-        >
-            <input
-                type="text"
-                x-ref="dateRangeInput"
-                x-on:click="init"
-                value="{{ $filter->getDateString(isset($this->appliedFilters[$filterKey]) ? $this->appliedFilters[$filterKey] : '') }}"
-                wire:key="{{ $filter->generateWireKey($tableName, 'dateRange') }}"
-                id="{{ $tableName }}-filter-dateRange-{{ $filterKey }}"
-                @class([
-                    'w-full inline-block align-middle transition duration-150 ease-in-out border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600' => $isTailwind,
-                    'd-inline-block w-100 form-control transition duration-150 ease-in-out border border-gray rounded-sm shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600' => $isBootstrap,
-                ])
-                @if($filter->hasConfig('placeholder')) placeholder="{{ $filter->getConfig('placeholder') }}" @endif
-            />     
-        </div>
+            @if($filter->hasConfig('placeholder')) placeholder="{{ $filter->getConfig('placeholder') }}" @endif
+        />     
     </div>
 </div>
