30bfc711a387b7f187b30a4899682dcbb8120445
Joe <104938042+lrljoe@users.noreply.github.com>
2023-11-03 15:55:20 +0000

    V3 Adds missing tests for DateTimeFilter and DateFilter (#1527)
    * Add Missing DateFilter and DateTimeFilter Tests
    
    ---------
    
    Co-authored-by: lrljoe <lrljoe@users.noreply.github.com>

--- tests/Views/Filters/DateTimeFilterTest.php
+++ tests/Views/Filters/DateTimeFilterTest.php
@@ -2,6 +2,7 @@
 
 namespace Rappasoft\LaravelLivewireTables\Tests\Views\Filters;
 
+use DateTime;
 use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\Views\Filters\DateTimeFilter;
 
@@ -134,5 +135,15 @@ final class DateTimeFilterTest extends FilterTestCase
         $this->assertSame('livewire-tables::components.tools.filters.datetime', self::$filterInstance->getViewPath());
         self::$filterInstance->setCustomView('test-custom-filter-view');
         $this->assertSame('test-custom-filter-view', self::$filterInstance->getViewPath());
+    }
+
+    /**
+     * @test
+     */
+    public function test_can_get_filter_pills_value(): void
+    {
+        $dateTime = (new DateTime('now'));
+
+        $this->assertSame($dateTime->format('d M Y - H:i'), self::$filterInstance->getFilterPillValue($dateTime->format('Y-m-d\TH:i')));
     }
 }
