f30db50084eec83a3916a8f4833c3ecb406e337d
Joe <104938042+lrljoe@users.noreply.github.com>
2023-11-18 18:34:32 +0000

    Return this

--- src/Traits/Configuration/BladeManagementConfiguration.php
+++ src/Traits/Configuration/BladeManagementConfiguration.php
@@ -4,13 +4,24 @@ namespace Rappasoft\LaravelLivewireTables\Traits\Configuration;
 
 trait BladeManagementConfiguration
 {
-    public function setCustomBladeOfflinePath(string $offlineBladePath)
+
+    public function setCustomBladeOfflinePath(string $offlineBladePath): self
     {
         $this->offlineBladePath = $offlineBladePath;
+        
+        return $this;
+
     }
+<<<<<<< HEAD
+    
+    public function setCustomBladeDebugPath(string $debugBladePath): self
+=======
 
     public function setCustomBladeDebugPath(string $debugBladePath)
+>>>>>>> 20b71f6d2b3166cc1ecacf4b34ce0c661b70c5b1
     {
         $this->debugBladePath = $debugBladePath;
+
+        return $this;
     }
 }
