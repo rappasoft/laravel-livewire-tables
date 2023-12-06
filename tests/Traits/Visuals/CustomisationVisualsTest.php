7d2c1c23f44b41d7e1b859d25b14ca6116714876
Joe <104938042+lrljoe@users.noreply.github.com>
2023-12-06 21:30:38 +0000

    Update references, use a call method for EmptyTable

--- tests/Traits/Visuals/CustomisationVisualsTest.php
+++ tests/Traits/Visuals/CustomisationVisualsTest.php
@@ -3,22 +3,27 @@
 namespace Rappasoft\LaravelLivewireTables\Tests\Traits\Visuals;
 
 use Livewire\Livewire;
-use Rappasoft\LaravelLivewireTables\Tests\Http\Livewire\PetsTableCustomEmpty;
+use Rappasoft\LaravelLivewireTables\Tests\Http\Livewire\PetsTable;
 use Rappasoft\LaravelLivewireTables\Tests\TestCase;
 
 class CustomisationVisualsTest extends TestCase
 {
     /** @test */
-    public function can_customise_empty_message(): void
+    public function can_customise_empty_message_view(): void
     {
-        Livewire::test(PetsTableCustomEmpty::class)
+        Livewire::test(PetsTable::class)
             ->set('search', 'sdfsdfsdfadsfasdfasdd')
+            ->call('setCustomEmptyView', 'livewire-tables::tests.testCustomEmpty')
             ->assertDontSee('No items found')
             ->set('search', '')
+            ->call('setCustomEmptyView', 'livewire-tables::tests.testCustomEmpty')
             ->assertDontSee('No items found')
             ->set('search', '')
+            ->call('setCustomEmptyView', 'livewire-tables::tests.testCustomEmpty')
             ->assertDontSee('Test Custom Empty Message')
             ->set('search', 'sdfsdfsdfadsfasdfasdd')
+            ->call('setCustomEmptyView', 'livewire-tables::tests.testCustomEmpty')
             ->assertSee('Test Custom Empty Message');
     }
+
 }
