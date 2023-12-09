c1c8fe1a322744ad611c31e9c2b64e768f9576d3
Joe <104938042+lrljoe@users.noreply.github.com>
2023-12-09 21:41:33 +0000

    Change Helpers to Public

--- src/Views/Traits/Helpers/ColorColumnHelpers.php
+++ src/Views/Traits/Helpers/ColorColumnHelpers.php
@@ -7,40 +7,40 @@ use Illuminate\View\ComponentAttributeBag;
 trait ColorColumnHelpers
 {
     // TODO: Test
-    protected function getView(): string
+    public function getView(): string
     {
         return $this->view;
     }
 
     // TODO: Test
-    protected function getColor($row): string
+    public function getColor($row): string
     {
         return $this->hasColorCallback() ? app()->call($this->getColorCallback(), ['row' => $row]) : $this->getValue($row);
     }
 
     // TODO: Test
-    protected function getAttributeBag($row)
+    public function getAttributeBag($row)
     {
         return new ComponentAttributeBag($this->hasAttributesCallback() ? app()->call($this->getAttributesCallback(), ['row' => $row]) : []);
     }
 
-    protected function getColorCallback(): ?callable
+    public function getColorCallback(): ?callable
     {
         return $this->colorCallback;
     }
 
-    protected function hasColorCallback(): bool
+    public function hasColorCallback(): bool
     {
         return $this->colorCallback !== null;
     }
 
     // TODO: Test
-    protected function getAttributesCallback(): ?callable
+    public function getAttributesCallback(): ?callable
     {
         return $this->attributesCallback;
     }
 
-    protected function hasAttributesCallback(): bool
+    public function hasAttributesCallback(): bool
     {
         return $this->attributesCallback !== null;
     }
