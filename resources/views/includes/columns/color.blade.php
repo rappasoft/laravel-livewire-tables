f20c4d1de9b53ca6aaf658b9f182fe8e00b34ce3
Joe <104938042+lrljoe@users.noreply.github.com>
2023-12-20 18:29:57 +0000

    FixColorColumn-Bootstrap

--- resources/views/includes/columns/color.blade.php
+++ resources/views/includes/columns/color.blade.php
@@ -1,10 +1,11 @@
 <div @class([
                 'items-center content-center place-content-center place-items-center' => $isTailwind,
+                'p-4' => !$isTailwind
             ])
 >
     <div {{ $attributeBag->class([
             'h-6 w-6 rounded-md self-center' => $isTailwind && ($attributeBag['default'] ?? (empty($attributeBag['class']) || (!empty($attributeBag['class']) && ($attributeBag['default'] ?? false)))),
-            
+            'w-100 p-4 mb-4' => !$isTailwind && ($attributeBag['default'] ?? (empty($attributeBag['class']) || (!empty($attributeBag['class']) && ($attributeBag['default'] ?? false)))),
         ]) }}
         @style([
             "background-color: {$color}" => $color,
