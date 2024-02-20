25fa9e2e0f076e74d2403a2c89b45c6d7a171643
Joe <104938042+lrljoe@users.noreply.github.com>
2024-02-20 00:24:39 +0000

    Add WireLinkColumn

---
+++ src/Views/Traits/Core/HasActionCallback.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Views\Traits\Core;
+
+use Closure;
+use Rappasoft\LaravelLivewireTables\Views\{Column,Filter};
+
+trait HasActionCallback
+{
+    protected mixed $actionCallback = null;
+
+    public function action(callable $callback): self
+    {
+        $this->actionCallback = $callback;
+
+        return $this;
+    }
+
+    public function getActionCallback(): ?callable
+    {
+        return $this->actionCallback;
+    }
+
+    public function hasActionCallback(): bool
+    {
+        return $this->actionCallback !== null;
+    }
+
+}
