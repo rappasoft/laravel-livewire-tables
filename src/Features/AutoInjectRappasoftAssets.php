rappasoft/laravel-livewire-tables

commit 65ce4947ab58b77fb3e454423202edd17e414e49

V3 Frontend Asset Tweaks (#1371)
* Check for "original" property

* Adjusting order of request validations

* Updates to Asset Injection Methods

* Fix assertViewIs is not working (#67)
Contribs By: Jackson Tong <tongtoan2704@gmail.com

* Tweaks for Auto Injection Options

* Test Updates - Frontend Assets

* Add ThirdParty Basic Header Tests

* Add Initial Debuggable DTO Test

src/Features/AutoInjectRappasoftAssets.php
@@ -14,52 +14,61 @@ class AutoInjectRappasoftAssets extends ComponentHook
 
     public static bool $forceAssetInjection = false;
 
+    public static bool $shouldInjectRappasoftThirdPartyAssets = false;
+
+    public static bool $shouldInjectRappasoftAssets = false;
+
     public static function provide(): void
     {
+        static::$shouldInjectRappasoftAssets = config('livewire-tables.inject_core_assets_enabled', true);
+        static::$shouldInjectRappasoftThirdPartyAssets = config('livewire-tables.inject_third_party_assets_enabled', true);
+
         on('flush-state', function () {
             static::$hasRenderedAComponentThisRequest = false;
             static::$forceAssetInjection = false;
         });
 
-        if (config('livewire-tables.inject_assets', true) === false) {
-            return;
+        if (static::$shouldInjectRappasoftAssets || static::$shouldInjectRappasoftThirdPartyAssets) {
+
+            app('events')->listen(RequestHandled::class, function (RequestHandled $handled) {
+
+                if (! static::$shouldInjectRappasoftAssets && ! static::$shouldInjectRappasoftThirdPartyAssets) {
+                    return;
+                }
+
+                // If All Scripts Have Been Rendered - Return
+                if (
+                    (
+                        ! static::$shouldInjectRappasoftAssets || app(RappasoftFrontendAssets::class)->hasRenderedRappsoftTableScripts
+                    ) &&
+                    (
+                        ! static::$shouldInjectRappasoftThirdPartyAssets || app(RappasoftFrontendAssets::class)->hasRenderedRappsoftTableThirdPartyScripts
+                    )
+                ) {
+                    return;
+                }
+
+                if (! str($handled->response->headers->get('content-type'))->contains('text/html')) {
+                    return;
+                }
+
+                if (! method_exists($handled->response, 'status') || ! method_exists($handled->response, 'getContent') || ! method_exists($handled->response, 'setContent') || ! method_exists($handled->response, 'getOriginalContent') || ! property_exists($handled->response, 'original')) {
+                    return;
+                }
+
+                if ($handled->response->status() !== 200) {
+                    return;
+                }
+
+                $html = $handled->response->getContent();
+
+                if (str($html)->contains('</html>')) {
+                    $original = $handled->response->getOriginalContent();
+                    $handled->response->setContent(static::injectAssets($html));
+                    $handled->response->original = $original;
+                }
+            });
         }
-
-        app('events')->listen(RequestHandled::class, function ($handled) {
-
-            if (! static::$forceAssetInjection && config('livewire-tables.inject_assets', true) === false) {
-                return;
-            }
-            if (! str($handled->response->headers->get('content-type'))->contains('text/html')) {
-                return;
-            }
-            if (! method_exists($handled->response, 'status') || $handled->response->status() !== 200) {
-                return;
-            }
-
-            if (! method_exists($handled->response, 'getContent') || ! method_exists($handled->response, 'setContent')) {
-                return;
-            }
-
-            if ((! static::$hasRenderedAComponentThisRequest) && (! static::$forceAssetInjection)) {
-                return;
-            }
-            if (config('livewire-tables.inject_assets', true) === false && config('livewire-tables.inject_third_party_assets', true) === false) {
-                return;
-            }
-            if (config('livewire-tables.inject_assets', true) === true && app(RappasoftFrontendAssets::class)->hasRenderedRappsoftTableScripts && config('livewire-tables.inject_third_party_assets', true) === true && app(RappasoftFrontendAssets::class)->hasRenderedRappsoftTableThirdPartyScripts) {
-                return;
-            } elseif (config('livewire-tables.inject_assets', true) === true && app(RappasoftFrontendAssets::class)->hasRenderedRappsoftTableScripts) {
-                return;
-            } elseif (config('livewire-tables.inject_third_party_assets', true) === true && app(RappasoftFrontendAssets::class)->hasRenderedRappsoftTableThirdPartyScripts) {
-                return;
-            }
-            $html = $handled->response->getContent();
-
-            if (str($html)->contains('</html>')) {
-                $handled->response->setContent(static::injectAssets($html));
-            }
-        });
     }
 
     public function dehydrate(): void
@@ -69,25 +78,24 @@ class AutoInjectRappasoftAssets extends ComponentHook
 
     public static function injectAssets(mixed $html): string
     {
-        $rappasoftTableStyles = config('livewire-tables.inject_assets', true) ? RappasoftFrontendAssets::tableStyles() : '';
-        $rappasoftTableScripts = config('livewire-tables.inject_assets', true) ? RappasoftFrontendAssets::tableScripts() : '';
-        $rappasoftTableThirdPartyStyles = config('livewire-tables.inject_third_party_assets', true) ? RappasoftFrontendAssets::tableThirdPartyStyles() : '';
-        //$rappasoftTableThirdPartyStyles = '';
-        $rappasoftTableThirdPartyScripts = config('livewire-tables.inject_third_party_assets', true) ? RappasoftFrontendAssets::tableThirdPartyScripts() : '';
-        //$rappasoftTableThirdPartyScripts = '';
 
         $html = str($html);
+        $rappaStyles = ((static::$shouldInjectRappasoftAssets === true) ? RappasoftFrontendAssets::tableStyles() : '').' '.((static::$shouldInjectRappasoftThirdPartyAssets === true) ? RappasoftFrontendAssets::tableThirdPartyStyles() : '');
+        $rappaScripts = ((static::$shouldInjectRappasoftAssets === true) ? RappasoftFrontendAssets::tableScripts() : '').' '.((static::$shouldInjectRappasoftThirdPartyAssets === true) ? RappasoftFrontendAssets::tableThirdPartyScripts() : '');
 
         if ($html->test('/<\s*head(?:\s|\s[^>])*>/i') && $html->test('/<\s*\/\s*body\s*>/i')) {
+            static::$shouldInjectRappasoftAssets = static::$shouldInjectRappasoftThirdPartyAssets = false;
+
             return $html
-                ->replaceMatches('/(<\s*head(?:\s|\s[^>])*>)/i', '$1'.($rappasoftTableStyles.' '.$rappasoftTableThirdPartyStyles))
-                ->replaceMatches('/(<\s*\/\s*head\s*>)/i', ($rappasoftTableScripts.' '.$rappasoftTableThirdPartyScripts).'$1')
+                ->replaceMatches('/(<\s*head(?:\s|\s[^>])*>)/i', '$1'.$rappaStyles)
+                ->replaceMatches('/(<\s*\/\s*head\s*>)/i', $rappaScripts.'$1')
                 ->toString();
         }
+        static::$shouldInjectRappasoftAssets = static::$shouldInjectRappasoftThirdPartyAssets = false;
 
         return $html
-            ->replaceMatches('/(<\s*html(?:\s[^>])*>)/i', '$1'.($rappasoftTableStyles.' '.$rappasoftTableThirdPartyStyles))
-            ->replaceMatches('/(<\s*\/\s*head\s*>)/i', ($rappasoftTableScripts.' '.$rappasoftTableThirdPartyScripts).'$1')
+            ->replaceMatches('/(<\s*html(?:\s[^>])*>)/i', '$1'.$rappaStyles)
+            ->replaceMatches('/(<\s*\/\s*head\s*>)/i', $rappaScripts.'$1')
             ->toString();
     }
 }

config/livewire-tables.php
@@ -6,26 +6,34 @@ return [
      */
     'theme' => 'tailwind',
 
-    'cache_assets' => true,
     /**
-     * Enable or Disable automatic injection of assets
+     * Filter Frontend Asset Options
      */
-    'inject_assets' => true,
 
     /**
-     * Enable or Disable automatic injection of assets
+     * Cache Rappasoft Frontend Assets
      */
-    'inject_third_party_assets' => false,
+    'cache_assets' => false,
 
     /**
-     * Enable or Disable inclusion of published third-party assets
+     * Enable or Disable automatic injection of core assets
      */
-    'published_third_party_assets' => false,
+    'inject_core_assets_enabled' => true,
 
     /**
-     * Enable or Disable remote third-party assets
+     * Enable or Disable automatic injection of third-party assets
      */
-    'remote_third_party_assets' => true,
+    'inject_third_party_assets_enabled' => true,
+
+    /**
+     * Enable Blade Directives (Not required if automatically injecting or using bundler approaches)
+     */
+    'enable_blade_directives ' => false,
+
+    /**
+     * Filter Default Configuration Options
+     *
+     * */
 
     /**
      * Configuration options for DateFilter

resources/imports/laravel-livewire-tables-thirdparty.js
@@ -1,0 +1,2 @@
+import './css/laravel-livewire-tables-thirdparty.css'
+import './js/laravel-livewire-tables-thirdparty.min.js'

resources/imports/laravel-livewire-tables.js
@@ -1,0 +1,2 @@
+import './css/laravel-livewire-tables.css'
+import './js/laravel-livewire-tables.js'

src/LaravelLivewireTablesServiceProvider.php
@@ -24,6 +24,14 @@ class LaravelLivewireTablesServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'livewire-tables');
 
+        $this->consoleCommands();
+
+        (new RappasoftFrontendAssets)->boot();
+
+    }
+
+    public function consoleCommands()
+    {
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../resources/lang' => $this->app->langPath('livewire-tables'),
@@ -45,12 +53,6 @@ class LaravelLivewireTablesServiceProvider extends ServiceProvider
                 MakeCommand::class,
             ]);
         }
-
-        if (config('livewire-tables.inject_assets', true) === true) {
-
-            (new RappasoftFrontendAssets)->boot();
-        }
-
     }
 
     public function register(): void
@@ -58,10 +60,10 @@ class LaravelLivewireTablesServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(
             __DIR__.'/../config/livewire-tables.php', 'livewire-tables'
         );
-        if (config('livewire-tables.inject_assets', true) === true) {
 
-            (new RappasoftFrontendAssets)->register();
-            ComponentHookRegistry::register(AutoInjectRappasoftAssets::class);
-        }
+        (new RappasoftFrontendAssets)->register();
+
+        ComponentHookRegistry::register(AutoInjectRappasoftAssets::class);
+
     }
 }

src/Mechanisms/RappasoftFrontendAssets.php
@@ -31,16 +31,12 @@ class RappasoftFrontendAssets
             return Route::get($scriptPath, $handle);
         });
 
-        Blade::directive('rappasoftTableScripts', [static::class, 'rappasoftTableScripts']);
-
         // Set the CSS route for the core tables CSS
         app($this::class)->setRappasoftTableStylesRoute(function ($handle) {
             $stylesPath = '/rappasoft/laravel-livewire-tables/core.min.css';
 
             return Route::get($stylesPath, $handle);
         });
-
-        Blade::directive('rappasoftTableStyles', [static::class, 'rappasoftTableStyles']);
 
         // Set the JS route for the third party JS
         app($this::class)->setRappasoftTableThirdPartyScriptRoute(function ($handle) {
@@ -49,8 +45,6 @@ class RappasoftFrontendAssets
             return Route::get($scriptPath, $handle);
         });
 
-        Blade::directive('rappasoftTableThirdPartyScripts', [static::class, 'rappasoftTableThirdPartyScripts']);
-
         // Set the CSS route for the third party CSS
         app($this::class)->setRappasoftTableThirdPartyStylesRoute(function ($handle) {
             $stylesPath = '/rappasoft/laravel-livewire-tables/thirdparty.css';
@@ -58,16 +52,30 @@ class RappasoftFrontendAssets
             return Route::get($stylesPath, $handle);
         });
 
-        Blade::directive('rappasoftTableThirdPartyStyles', [static::class, 'rappasoftTableThirdPartyStyles']);
+        static::registerBladeDirectives();
 
+    }
+
+    protected function registerBladeDirectives()
+    {
+        Blade::directive('rappasoftTableScripts', [static::class, 'rappasoftTableScripts']);
+        Blade::directive('rappasoftTableStyles', [static::class, 'rappasoftTableStyles']);
+        Blade::directive('rappasoftTableThirdPartyScripts', [static::class, 'rappasoftTableThirdPartyScripts']);
+        Blade::directive('rappasoftTableThirdPartyStyles', [static::class, 'rappasoftTableThirdPartyStyles']);
     }
 
     protected function pretendResponseIsJs(string $file): \Symfony\Component\HttpFoundation\Response
     {
 
-        $expires = strtotime((config('livewire-tables.cache_assets', false) ? '+1 second' : '+1 hour'));
-        $lastModified = (config('livewire-tables.cache_assets', false) ? strtotime(now()) : filemtime($file));
-        $cacheControl = (config('livewire-tables.cache_assets', false) ? 'public, max-age=1' : 'public, max-age=3600');
+        if (config('livewire-tables.cache_assets', false) === true) {
+            $expires = strtotime('+1 hour');
+            $lastModified = filemtime($file);
+            $cacheControl = 'public, max-age=3600';
+        } else {
+            $expires = strtotime('+1 second');
+            $lastModified = \Carbon\Carbon::now()->timestamp;
+            $cacheControl = 'public, max-age=1';
+        }
 
         $headers = [
             'Content-Type' => 'application/javascript; charset=utf-8',
@@ -81,9 +89,15 @@ class RappasoftFrontendAssets
 
     protected function pretendResponseIsCSS(string $file): \Symfony\Component\HttpFoundation\Response
     {
-        $expires = strtotime((config('livewire-tables.cache_assets', false) ? '+1 second' : '+1 hour'));
-        $lastModified = (config('livewire-tables.cache_assets', false) ? strtotime(now()) : filemtime($file));
-        $cacheControl = (config('livewire-tables.cache_assets', false) ? 'public, max-age=1' : 'public, max-age=3600');
+        if (config('livewire-tables.cache_assets', false) === true) {
+            $expires = strtotime('+1 hour');
+            $lastModified = filemtime($file);
+            $cacheControl = 'public, max-age=3600';
+        } else {
+            $expires = strtotime('+1 second');
+            $lastModified = \Carbon\Carbon::now()->timestamp;
+            $cacheControl = 'public, max-age=1';
+        }
 
         $headers = [
             'Content-Type' => 'text/css; charset=utf-8',

src/Traits/Mechanisms/WithRappasoftTableScripts.php
@@ -50,7 +50,7 @@ trait WithRappasoftTableScripts
         $scripts = static::tableJs($options);
 
         // HTML Label.
-        $html = $debug ? ['<!-- Rappasoft Table Scripts -->'] : [];
+        $html = $debug ? ['<!-- Rappasoft Core Table Scripts -->'] : [];
 
         $html[] = $scripts;
 
@@ -75,7 +75,7 @@ trait WithRappasoftTableScripts
         );
 
         return <<<HTML
-        <script src="{$url}"  {$nonce} {$extraAttributes}></script>
+        <script src="{$url}" {$nonce} {$extraAttributes}></script>
         HTML;
     }
 }

src/Traits/Mechanisms/WithRappasoftTableStyles.php
@@ -43,7 +43,7 @@ trait WithRappasoftTableStyles
         $styles = static::tableCss($options);
 
         // HTML Label.
-        $html = $debug ? ['<!-- Rappasoft Table Styles -->'] : [];
+        $html = $debug ? ['<!-- Rappasoft Core Table Styles -->'] : [];
 
         $html[] = $styles;
 

commit 44bb82a6ca27a74dc6cb639312cc006ca380d9aa

[Bug] Auto Inject Assets: Only if component rendered
Regression from 65ce4947ab58b77fb3e454423202edd17e414e49 #1371

Fixes: #1587

Check if component is instanceof DataTableComponent so as not to inject on every livewire page, only ones with a datatable component

src/Features/AutoInjectRappasoftAssets.php
@@ -4,6 +4,7 @@ namespace Rappasoft\LaravelLivewireTables\Features;
 
 use Illuminate\Foundation\Http\Events\RequestHandled;
 use Livewire\ComponentHook;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Mechanisms\RappasoftFrontendAssets;
 
 use function Livewire\on;
@@ -33,6 +34,10 @@ class AutoInjectRappasoftAssets extends ComponentHook
             app('events')->listen(RequestHandled::class, function (RequestHandled $handled) {
 
                 if (! static::$shouldInjectRappasoftAssets && ! static::$shouldInjectRappasoftThirdPartyAssets) {
+                    return;
+                }
+
+                if (! static::$hasRenderedAComponentThisRequest) {
                     return;
                 }
 
@@ -73,7 +78,9 @@ class AutoInjectRappasoftAssets extends ComponentHook
 
     public function dehydrate(): void
     {
-        static::$hasRenderedAComponentThisRequest = true;
+        if ($this->component instanceof DataTableComponent){
+            static::$hasRenderedAComponentThisRequest = true;
+        }
     }
 
     public static function injectAssets(mixed $html): string