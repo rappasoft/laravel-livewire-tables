28d27200beb92b9ae7c4f2d64acb88bfee2b28c1
lrljoe <lrljoe@users.noreply.github.com>
2024-05-30 00:50:37 +0000

    Fix styling

--- src/Views/Columns/DateColumn.php
+++ src/Views/Columns/DateColumn.php
@@ -28,29 +28,29 @@ class DateColumn extends Column
     {
 
         $dateTime = $this->getValue($row);
-        if ($dateTime != '' && $dateTime != null)
-        {
+        if ($dateTime != '' && $dateTime != null) {
             if (! ($dateTime instanceof \DateTime)) {
                 try {
                     // Check if format matches what is expected
                     if (! \Carbon\Carbon::hasFormatWithModifiers($dateTime, $this->getInputFormat())) {
                         throw new \Exception('DateColumn Received Invalid Format');
                     }
-    
+
                     // Create DateTime Object
                     $dateTime = \DateTime::createFromFormat($this->getInputFormat(), $dateTime);
                 } catch (\Exception $exception) {
                     report($exception);
-    
+
                     // Return Null
                     return $this->getEmptyValue();
                 }
             }
-    
+
             // Return
             return $dateTime->format($this->getOutputFormat());
-    
+
         }
+
         return $this->getEmptyValue();
     }
 }
