9d3b2bda20f4d5b8f37f04b9460cdeae899a15c6
Joe <104938042+lrljoe@users.noreply.github.com>
2024-07-05 22:26:20 +0100

    Add SumColumn

---
+++ src/Views/Columns/SumColumn.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Views\Columns;
+
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\Views\Traits\Configuration\sumColumnConfiguration;
+use Rappasoft\LaravelLivewireTables\Views\Traits\Helpers\SumColumnHelpers;
+use Rappasoft\LaravelLivewireTables\Views\Traits\IsColumn;
+
+class SumColumn extends Column
+{
+    use IsColumn,
+        SumColumnHelpers,
+        SumColumnConfiguration { SumColumnConfiguration::sortable insteadof IsColumn; }
+
+    public ?string $dataSource;
+
+    public string $aggregateMethod = 'sum';
+
+    public function __construct(string $title, ?string $from = null)
+    {
+        parent::__construct($title, $from);
+        if (! isset($from)) {
+            $this->label(fn () => null);
+        }
+    }
+}
---
+++ src/Views/Traits/Helpers/SumColumnHelpers.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Views\Traits\Helpers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\View\ComponentAttributeBag;
+
+trait SumColumnHelpers
+{
+    public function getDataSource(): string
+    {
+
+        return $this->dataSource;
+    }
+
+    public function hasDataSource(): bool
+    {
+        return isset($this->dataSource);
+    }
+
+    public function getAggregateMethod(): string
+    {
+        return $this->aggregateMethod;
+    }
+}
