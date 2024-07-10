0bc1c01a7de5c9ff33166877a873bf2f82d9555d
lrljoe <joe@lowerrocklabs.com>
2024-07-10 23:43:30 +0100

    Add ArrayColumnTest

--- tests/Views/Columns/ArrayColumnTest.php
+++ tests/Views/Columns/ArrayColumnTest.php
@@ -5,6 +5,7 @@ namespace Rappasoft\LaravelLivewireTables\Tests\Views\Columns;
 use Rappasoft\LaravelLivewireTables\Tests\Models\Pet;
 use Rappasoft\LaravelLivewireTables\Tests\TestCase;
 use Rappasoft\LaravelLivewireTables\Views\Columns\ArrayColumn;
+use Rappasoft\LaravelLivewireTables\Exceptions\DataTableConfigurationException;
 
 final class ArrayColumnTest extends TestCase
 {
@@ -14,4 +15,48 @@ final class ArrayColumnTest extends TestCase
 
         $this->assertSame('Array Col', $column->getTitle());
     }
-}
+
+    public function test_can_set_the_separator(): void
+    {
+        $column = ArrayColumn::make('Array Col');
+
+        $this->assertSame('<br />', $column->getSeparator());
+        $column->separator('<br /><br />');
+        $this->assertTrue($column->hasSeparator());
+
+        $this->assertSame('<br /><br />', $column->getSeparator());
+    }
+
+    public function test_can_set_the_output_format(): void
+    {
+        $column = ArrayColumn::make('Array Col');
+
+        $this->assertNull($column->getOutputFormatCallback());
+        $this->assertFalse($column->hasOutputFormatCallback());
+        $column->outputFormat(fn($index, $value) => "<a href='".$value->id."'>".$value->name."</a>");
+        $this->assertTrue($column->hasOutputFormatCallback());
+    }
+
+    public function test_requires_the_data_callback(): void
+    {
+        $this->expectException(DataTableConfigurationException::class);
+        $column = ArrayColumn::make('Average Age')
+            ->separator('<br /><br />')
+            ->sortable();
+        $contents = $column->getContents(Pet::find(1));
+        $this->assertNull($contents);
+    }
+
+
+    public function test_requires_the_output_format_callback(): void
+    {
+        $this->expectException(DataTableConfigurationException::class);
+        $column = ArrayColumn::make('Average Age')
+            ->separator('<br /><br />')
+            ->data(fn($value, $row) => ($row->pets))
+            ->sortable();
+        $contents = $column->getContents(Pet::find(1));
+        $this->assertNull($contents);
+    }
+
+}    
