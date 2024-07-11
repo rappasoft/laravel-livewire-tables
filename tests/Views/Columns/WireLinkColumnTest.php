4286bc14dbd5c076d7dcde09c6d5116571f20445
Joe <104938042+lrljoe@users.noreply.github.com>
2024-07-11 01:33:30 +0100

    Add Tests for WireLinkColumn

--- tests/Views/Columns/WireLinkColumnTest.php
+++ tests/Views/Columns/WireLinkColumnTest.php
@@ -9,51 +9,61 @@ use Rappasoft\LaravelLivewireTables\Views\Columns\WireLinkColumn;
 
 class WireLinkColumnTest extends TestCase
 {
-    /** @test */
-    public function can_set_the_column_title(): void
+    public function test_can_set_the_column_title(): void
     {
         $column = WireLinkColumn::make('Name', 'name');
 
         $this->assertSame('Name', $column->getTitle());
     }
 
-    /** @test */
-    public function can_not_infer_field_name_from_title_if_no_from(): void
+    public function test_can_not_infer_field_name_from_title_if_no_from(): void
     {
         $column = WireLinkColumn::make('My Title');
 
         $this->assertNull($column->getField());
     }
 
-    /** @test */
-    public function can_not_render_field_if_no_title_callback(): void
+    public function test_can_not_render_field_if_no_title_callback(): void
     {
         $this->expectException(DataTableConfigurationException::class);
 
         WireLinkColumn::make('Name')->getContents(Pet::find(1));
     }
 
-    /** @test */
-    public function can_not_render_field_if_no_action_callback(): void
+    public function test_can_not_render_field_if_no_action_callback(): void
     {
         $this->expectException(DataTableConfigurationException::class);
 
         WireLinkColumn::make('Name')->title(fn ($row) => 'Edit')->getContents(Pet::find(1));
     }
 
-    /** @test */
-    public function can_render_field_if_title_and_action_callback(): void
+    public function test_can_render_field_if_title_and_action_callback(): void
     {
         $column = WireLinkColumn::make('Name')->title(fn ($row) => 'Edit')->action(fn ($row) => 'delete("'.$row->id.'")')->getContents(Pet::find(1));
 
         $this->assertNotEmpty($column);
     }
 
-    /** @test */
-    public function can_render_field_if_confirm_set(): void
+    public function test_can_render_field_if_confirm_set(): void
     {
         $column = WireLinkColumn::make('Name')->title(fn ($row) => 'Edit')->action(fn ($row) => 'delete("'.$row->id.'")')->confirmMessage('Test')->getContents(Pet::find(1));
 
         $this->assertNotEmpty($column);
     }
+
+    public function test_can_add_confirm_message(): void
+    {
+        $column = WireLinkColumn::make('Name', 'name');
+
+        $this->assertFalse($column->hasConfirmMessage());
+
+        $this->assertNull($column->getConfirmMessage())
+
+        $column->confirmMessage('Test');
+
+        $this->assertTrue($column->hasConfirmMessage());
+
+        $this->assertSame('Test',$column->getConfirmMessage())
+    }
+
 }
