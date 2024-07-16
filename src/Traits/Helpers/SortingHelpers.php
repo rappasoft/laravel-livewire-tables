0548e4453e6650b135f7d2aa64d8d1ae84274282
lrljoe <lrljoe@users.noreply.github.com>
2024-07-16 05:40:30 +0000

    Fix styling

--- src/Traits/Helpers/SortingHelpers.php
+++ src/Traits/Helpers/SortingHelpers.php
@@ -18,17 +18,14 @@ trait SortingHelpers
 
     public function getSorts(): array
     {
-        foreach($this->sorts as $column => $direction)
-        {
-            if (is_array($direction))
-            {
-                foreach($direction as $colAppend => $actualDirection)
-                {
-                    $this->sorts[$column.".".$colAppend] = $actualDirection;
+        foreach ($this->sorts as $column => $direction) {
+            if (is_array($direction)) {
+                foreach ($direction as $colAppend => $actualDirection) {
+                    $this->sorts[$column.'.'.$colAppend] = $actualDirection;
                     unset($this->sorts[$column]);
                 }
             }
-    
+
         }
 
         return $this->sorts;
