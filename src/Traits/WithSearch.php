61b1563432ded714b061a3e12165e551c364badd
lrljoe <joe@lowerrocklabs.com>
2024-07-27 18:06:41 +0100

    Add Search Lifecycle Hook

--- src/Traits/WithSearch.php
+++ src/Traits/WithSearch.php
@@ -49,6 +49,9 @@ trait WithSearch
     {
         if ($this->searchIsEnabled() && $this->hasSearch()) {
             $searchableColumns = $this->getSearchableColumns();
+            
+            $this->callHook('searchUpdated', ['value' => $this->getSearch()]);
+            $this->callTraitHook('searchUpdated', ['value' => $this->getSearch()]);
 
             if ($searchableColumns->count()) {
                 $this->setBuilder($this->getBuilder()->where(function ($query) use ($searchableColumns) {
