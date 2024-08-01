b35a64bd3f9a754c50a19244cacbe08aea3c3252
Joe <104938042+lrljoe@users.noreply.github.com>
2024-08-01 02:18:05 +0100

    Initial Commit

--- src/Traits/Configuration/ReorderingConfiguration.php
+++ src/Traits/Configuration/ReorderingConfiguration.php
@@ -81,4 +81,16 @@ trait ReorderingConfiguration
 
         return $this;
     }
+
+    /**
+     * Used to set attributes for the <th> for Reorder Column
+     */
+    public function setReorderThAttributes(array $reorderThAttributes): self
+    {
+        $this->reorderThAttributes = [...$this->reorderThAttributes, ...$reorderThAttributes];
+
+        return $this;
+    }
+
+
 }
