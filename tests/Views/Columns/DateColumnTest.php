06bb8950a2d033092ca55315c58e96ec652e28c8
lrljoe <joe@lowerrocklabs.com>
2024-08-03 07:57:01 +0100

    Remove Laravel 11 Exceptions Report Handler

--- tests/Views/Columns/DateColumnTest.php
+++ tests/Views/Columns/DateColumnTest.php
@@ -2,7 +2,7 @@
 
 namespace Rappasoft\LaravelLivewireTables\Tests\Views\Columns;
 
-use Illuminate\Support\Facades\Exceptions;
+//use Illuminate\Support\Facades\Exceptions;
 use Rappasoft\LaravelLivewireTables\Tests\Models\Pet;
 use Rappasoft\LaravelLivewireTables\Tests\TestCase;
 use Rappasoft\LaravelLivewireTables\Views\Columns\DateColumn;
@@ -81,13 +81,13 @@ final class DateColumnTest extends TestCase
 
         $firstRow->last_visit = '44-12-2023';
 
-        Exceptions::fake();
+      //  Exceptions::fake();
 
         $this->assertSame('Unknown', $column->emptyValue('Unknown')->getContents($firstRow));
 
-        Exceptions::assertReported(function (\Carbon\Exceptions\InvalidFormatException $e): bool {
-            return $e->getMessage() === "Could not parse '44-12-2023': Failed to parse time string (44-12-2023) at position 8 (2): Unexpected character";
-        });
+       // Exceptions::assertReported(function (\Carbon\Exceptions\InvalidFormatException $e): bool {
+       //     return $e->getMessage() === "Could not parse '44-12-2023': Failed to parse time string (44-12-2023) at position 8 (2): Unexpected character";
+       // });
 
     }
 
