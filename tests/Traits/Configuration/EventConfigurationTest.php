b90c2319d64a7419d1f2074a7ee960a4b9126b19
Joe <104938042+lrljoe@users.noreply.github.com>
2024-08-04 07:10:01 +0100

    Add two new test classes

---
+++ tests/Traits/Configuration/EventConfigurationTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Tests\Traits\Configuration;
+
+use Rappasoft\LaravelLivewireTables\Tests\TestCase;
+
+final class EventConfigurationTest extends TestCase
+{
+
+
+}
