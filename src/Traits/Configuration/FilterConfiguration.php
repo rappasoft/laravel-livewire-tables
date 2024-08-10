fa17ef20dc4f5220f84cbcbd3bb9e6877d4dbb00
Joe <104938042+lrljoe@users.noreply.github.com>
2024-08-10 05:45:02 +0100

    FilterGenericData Update

--- src/Traits/Configuration/FilterConfiguration.php
+++ src/Traits/Configuration/FilterConfiguration.php
@@ -118,7 +118,7 @@ trait FilterConfiguration
 
     public function generateFilterGenericData(): array
     {
-        return (new FilterGenericData($this))->toArray();
+        return (new FilterGenericData($this->getTableName(), $this->getFilterLayout(), $this->isTailwind(), $this->isBootstrap4(), $this->isBootstrap5()))->toArray();
     }
 
     public function setFilterGenericData(array $filterGenericData = []): void
