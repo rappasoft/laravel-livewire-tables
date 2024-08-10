38206ecf6c03b80984b895b05e0d8ca8d2fccbfe
lrljoe <joe@lowerrocklabs.com>
2024-08-10 01:58:50 +0100

    Simplify jS

--- resources/views/components/wrapper.blade.php
+++ resources/views/components/wrapper.blade.php
@@ -1,11 +1,11 @@
 @props(['component', 'tableName'])
-
-<div wire:key="{{ $tableName }}-wrapper" x-data="tableWrapper($wire, {{ $component->showBulkActionsDropdownAlpine() }})">
+@php($tableId = $component->getTableAttributes()['id'])
+<div wire:key="{{ $tableName }}-wrapper" x-data="tableWrapper($wire, '{{ $component->showBulkActionsDropdownAlpine() }}', '{{ $tableId }}', '{{ $component->getPrimaryKey() }}')">
     <div {{ $attributes->merge($this->getComponentWrapperAttributes()) }}
         @if ($component->hasRefresh()) wire:poll{{ $component->getRefreshOptions() }} @endif
         @if ($component->isFilterLayoutSlideDown()) wire:ignore.self @endif>
 
-        <div x-data="reorderFunction($wire, '{{ $component->getTableAttributes()['id'] }}', '{{ $component->getPrimaryKey() }}')">
+        <div>
         @if ($component->debugIsEnabled())
             @include('livewire-tables::includes.debug')
         @endif
