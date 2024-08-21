5368adfed724f20a3a7195489c955c630b9ad1f9
lrljoe <lrljoe@users.noreply.github.com>
2024-08-21 21:57:29 +0000

    Fix styling

--- tests/Views/Actions/ActionTest.php
+++ tests/Views/Actions/ActionTest.php
@@ -4,10 +4,10 @@ namespace Rappasoft\LaravelLivewireTables\Tests\Views\Actions;
 
 use Illuminate\View\ComponentAttributeBag;
 use Rappasoft\LaravelLivewireTables\Exceptions\DataTableConfigurationException;
+use Rappasoft\LaravelLivewireTables\Tests\Http\Livewire\{PetsTable,PetsTableAttributes};
 use Rappasoft\LaravelLivewireTables\Tests\Models\Pet;
 use Rappasoft\LaravelLivewireTables\Tests\TestCase;
 use Rappasoft\LaravelLivewireTables\Views\Actions\Action;
-use Rappasoft\LaravelLivewireTables\Tests\Http\Livewire\{PetsTable,PetsTableAttributes};
 
 final class ActionTest extends TestCase
 {
@@ -55,7 +55,7 @@ final class ActionTest extends TestCase
 
         $action->setIconAttributes(['class' => 'font-sm text-sm']);
         $bag = new \Illuminate\View\ComponentAttributeBag(['default-styling' => true, 'class' => 'font-sm text-sm']);
-        
+
         $this->assertSame($bag->getAttributes(), $action->getIconAttributes()->getAttributes());
         $this->assertSame(['default-styling' => true, 'class' => 'font-sm text-sm'], $action->iconAttributes);
 
@@ -182,9 +182,9 @@ final class ActionTest extends TestCase
 
     public function test_can_set_action_wrapper_attributes(): void
     {
-        $petsTable = (new class extends PetsTable {
+        $petsTable = (new class extends PetsTable
+        {
             use \Rappasoft\LaravelLivewireTables\Traits\WithActions;
-
 
             public function configure(): void
             {
@@ -204,14 +204,14 @@ final class ActionTest extends TestCase
         $this->assertSame(['default-styling' => true, 'default-colors' => true], $petsTable->getActionWrapperAttributes());
         $petsTable->setActionWrapperAttributes(['default-styling' => false, 'class' => 'bg-blue-500']);
         $this->assertSame([
-            'default-styling' => false, 
+            'default-styling' => false,
             'default-colors' => true,
             'class' => 'bg-blue-500',
         ], $petsTable->getActionWrapperAttributes());
 
         $petsTable->setActionWrapperAttributes(['default-colors' => false, 'class' => 'bg-red-500']);
         $this->assertSame([
-            'default-styling' => true, 
+            'default-styling' => true,
             'default-colors' => false,
             'class' => 'bg-red-500',
         ], $petsTable->getActionWrapperAttributes());
@@ -220,13 +220,15 @@ final class ActionTest extends TestCase
 
     public function test_can_check_has_actions(): void
     {
-        $petsTable = (new class extends PetsTable {
+        $petsTable = (new class extends PetsTable
+        {
             use \Rappasoft\LaravelLivewireTables\Traits\WithActions;
 
-            public function actions() {
+            public function actions()
+            {
                 return [
                     \Rappasoft\LaravelLivewireTables\Views\Actions\Action::make('Test Edit 1')
-            ->setRoute('dashboard24')
+                        ->setRoute('dashboard24'),
                 ];
             }
 
@@ -250,5 +252,4 @@ final class ActionTest extends TestCase
         $this->assertSame(1, $petsTable->getActions()->count());
 
     }
-
 }
