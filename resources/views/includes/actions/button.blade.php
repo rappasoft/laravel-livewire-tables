38eedc19912089377ee235a26e5f011a07b68e3a
Joe <104938042+lrljoe@users.noreply.github.com>
2024-08-23 20:09:43 +0100

    v3.4.11 (#1878)
    ## [v3.4.11] - 2024-08-23
    ### New Features
    - Add setIconLeft/setIconRight for Actions by @lrljoe in https://github.com/rappasoft/laravel-livewire-tables/pull/1877

--- resources/views/includes/actions/button.blade.php
+++ resources/views/includes/actions/button.blade.php
@@ -1,5 +1,5 @@
 <a {{ $attributes->merge()
-            ->class(['justify-center text-center items-center inline-flex rounded-md border shadow-sm px-4 py-2 text-sm font-medium focus:ring focus:ring-opacity-50' => $isTailwind && $attributes['default-styling'] ?? true])
+            ->class(['justify-center text-center items-center inline-flex space-x-2 rounded-md border shadow-sm px-4 py-2 text-sm font-medium focus:ring focus:ring-opacity-50' => $isTailwind && $attributes['default-styling'] ?? true])
             ->class(['focus:border-indigo-300 focus:ring-indigo-200' => $isTailwind && $attributes['default-colors'] ?? true])
             ->class(['btn btn-sm btn-success' => $isBootstrap && $attributes['default-styling'] ?? true])
             ->class(['' => $isBootstrap && $attributes['default-colors'] ?? true])
@@ -12,13 +12,22 @@
             wire:navigate
            @endif
         >
-            {{ $action->getLabel() }}
 
-        @if($action->hasIcon())
+        @if($action->hasIcon() && $action->getIconRight())
+            <span>{{ $action->getLabel() }}</span>
             <i {{ $action->getIconAttributes()
-                ->class(["ms-1 ". $action->getIcon() => $isBootstrap])
-                ->class(["ml-1 ". $action->getIcon() => $isTailwind])
-                ->except('default-styling')
-            }}></i>
+                    ->class(["ms-1 ". $action->getIcon() => $isBootstrap])
+                    ->class(["ml-1 ". $action->getIcon() => $isTailwind])
+                    ->except('default-styling')
+                }}
+            ></i>
+        @elseif($action->hasIcon() && !$action->getIconRight())
+            <i {{ $action->getIconAttributes()
+                    ->class(["ms-1 ". $action->getIcon() => $isBootstrap])
+                    ->class(["ml-1 ". $action->getIcon() => $isTailwind])
+                    ->except('default-styling')
+                }}
+            ></i>
+            <span>{{ $action->getLabel() }}</span>
         @endif
 </a>
