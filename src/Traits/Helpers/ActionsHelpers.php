0b0d9cb5e3a2a9713b56833b72550c2fe772ea72
Joe <104938042+lrljoe@users.noreply.github.com>
2024-08-27 00:48:57 +0100

    Fix has actions (#1901)
    * Fix Action Repetitive Calling
    
    * Fix validActionsCount
    
    ---------
    
    Co-authored-by: lrljoe <lrljoe@users.noreply.github.com>

--- src/Traits/Helpers/ActionsHelpers.php
+++ src/Traits/Helpers/ActionsHelpers.php
@@ -29,18 +29,24 @@ trait ActionsHelpers
     #[Computed]
     public function hasActions(): bool
     {
-        return (new Collection($this->actions()))
-            ->filter(fn ($action) => $action instanceof Action)->count() > 0;
+        if (! isset($this->validActions)) {
+            $this->validActions = $this->getActions();
+        }
+
+        return $this->validActions->count() > 0;
     }
 
     #[Computed]
     public function getActions(): Collection
     {
-        return (new Collection($this->actions()))
-            ->filter(fn ($action) => $action instanceof Action)
-            ->each(function (Action $action, int $key) {
-                $action->setTheme($this->getTheme());
-            });
+        if (! isset($this->validActions)) {
+            $this->validActions = (new Collection($this->actions()))
+                ->filter(fn ($action) => $action instanceof Action)
+                ->each(function (Action $action, int $key) {
+                    $action->setTheme($this->getTheme());
+                });
+        }
 
+        return $this->validActions;
     }
 }
