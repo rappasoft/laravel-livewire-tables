aecb267cda9b05621afbaedff74e6c5937162d96
lrljoe <lrljoe@users.noreply.github.com>
2024-08-27 05:03:07 +0000

    Fix styling

--- tests/Traits/Visuals/Columns/IconColumnVisualsTest.php
+++ tests/Traits/Visuals/Columns/IconColumnVisualsTest.php
@@ -28,8 +28,13 @@ final class IconColumnVisualsTest extends TestCase
                 return [
                     \Rappasoft\LaravelLivewireTables\Views\Column::make('Name')->searchable(),
                     \Rappasoft\LaravelLivewireTables\Views\Columns\IconColumn::make('Old Age', 'age')
-                    ->setIcon(function (\Rappasoft\LaravelLivewireTables\Tests\Models\Pet $row, int $value) { 
-                        if ($value >= 5) { return 'heroicon-o-check-circle'; } else { return 'heroicon-o-x-circle';}}),
+                        ->setIcon(function (\Rappasoft\LaravelLivewireTables\Tests\Models\Pet $row, int $value) {
+                            if ($value >= 5) {
+                                return 'heroicon-o-check-circle';
+                            } else {
+                                return 'heroicon-o-x-circle';
+                            }
+                        }),
                 ];
             }
 
@@ -38,22 +43,22 @@ final class IconColumnVisualsTest extends TestCase
                 return [];
             }
         })
-        ->call('setSearch','Cartman')
-        ->assertSeeHtmlInOrder([
-            '<div class="livewire-tables-columns-icon">',
-            '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">',
-            '<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>',
-            '</svg></div>',
-        ])
-        ->assertDontSeeHtml('<path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>')
-        ->call('setSearch','May')
-        ->assertDontSeeHtml('<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>')
-        ->assertSeeHtmlInOrder([
-            '<div class="livewire-tables-columns-icon">',
-            '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">',
-            '<path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>',
-            '</svg></div>',
-        ]);
+            ->call('setSearch', 'Cartman')
+            ->assertSeeHtmlInOrder([
+                '<div class="livewire-tables-columns-icon">',
+                '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">',
+                '<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>',
+                '</svg></div>',
+            ])
+            ->assertDontSeeHtml('<path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>')
+            ->call('setSearch', 'May')
+            ->assertDontSeeHtml('<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>')
+            ->assertSeeHtmlInOrder([
+                '<div class="livewire-tables-columns-icon">',
+                '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">',
+                '<path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>',
+                '</svg></div>',
+            ]);
 
     }
 }
