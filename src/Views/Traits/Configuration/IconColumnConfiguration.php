51dbe8ceaaf2913c0e8597d631c12c6c9cc00f15
lrljoe <joe@lowerrocklabs.com>
2024-08-27 03:45:46 +0100

    Initial Commit - Awaiting Docs & Tests

---
+++ src/Views/Traits/Configuration/IconColumnConfiguration.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Views\Traits\Configuration;
+
+trait IconColumnConfiguration
+{
+    public function setIcon(\Closure $callback): self
+    {
+        $this->iconCallback = $callback;
+
+        return $this;
+    }
+
+}
--- src/Views/Traits/Core/HasAttributes.php
+++ src/Views/Traits/Core/HasAttributes.php
@@ -29,9 +29,15 @@ trait HasAttributes
     }
 
     // TODO: Test
+    public function getAttributes(Model $row): array
+    {
+        return app()->call($this->getAttributesCallback(), ['row' => $row, 'value' => $this->getValue($row) ?? '']) ?? [];
+    }
+
+    // TODO: Test
     public function getAttributeBag(Model $row): ComponentAttributeBag
     {
-        return new ComponentAttributeBag($this->hasAttributesCallback() ? app()->call($this->getAttributesCallback(), ['row' => $row]) : []);
+        return new ComponentAttributeBag($this->hasAttributesCallback() ? app()->call($this->getAttributesCallback(), ['row' => $row, 'value' => $this->getValue($row) ?? '']) : []);
     }
 
     /**
