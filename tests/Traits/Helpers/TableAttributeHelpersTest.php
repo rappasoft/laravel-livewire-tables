e124759d5e457a05bcf1e64f5a7fa4747e80eca7
lrljoe <lrljoe@users.noreply.github.com>
2024-09-01 18:00:08 +0000

    Fix styling

--- tests/Traits/Helpers/TableAttributeHelpersTest.php
+++ tests/Traits/Helpers/TableAttributeHelpersTest.php
@@ -12,19 +12,18 @@ final class TableAttributeHelpersTest extends TestCase
         $this->assertSame('shouldBeDisplayed', $topLevelAttributesArray['x-show']);
         $this->assertSame("showTable(event.detail.tableName ?? '', event.detail.tableFingerpint ?? '')", $topLevelAttributesArray['x-on:show-table.window']);
         $this->assertSame("hideTable(event.detail.tableName ?? '', event.detail.tableFingerpint ?? '')", $topLevelAttributesArray['x-on:hide-table.window']);
-        $this->assertSame("laravellivewiretable(\$wire, '". $this->basicTable->showBulkActionsDropdownAlpine()."', '".$this->basicTable->getTableId()."', '". $this->basicTable->getPrimaryKey()."')", $topLevelAttributesArray['x-data']);
+        $this->assertSame("laravellivewiretable(\$wire, '".$this->basicTable->showBulkActionsDropdownAlpine()."', '".$this->basicTable->getTableId()."', '".$this->basicTable->getPrimaryKey()."')", $topLevelAttributesArray['x-data']);
     }
 
     public function test_top_level_attribute_bag_matches(): void
     {
         $topLevelAttributeBag = $this->basicTable->getTopLevelAttributes();
-        
+
         $topLevelAttributesArray = $topLevelAttributeBag->getAttributes();
 
         $this->assertSame('shouldBeDisplayed', $topLevelAttributesArray['x-show']);
         $this->assertSame("showTable(event.detail.tableName ?? '', event.detail.tableFingerpint ?? '')", $topLevelAttributesArray['x-on:show-table.window']);
         $this->assertSame("hideTable(event.detail.tableName ?? '', event.detail.tableFingerpint ?? '')", $topLevelAttributesArray['x-on:hide-table.window']);
-        $this->assertSame("laravellivewiretable(\$wire, '". $this->basicTable->showBulkActionsDropdownAlpine()."', '".$this->basicTable->getTableId()."', '". $this->basicTable->getPrimaryKey()."')", $topLevelAttributesArray['x-data']);
+        $this->assertSame("laravellivewiretable(\$wire, '".$this->basicTable->showBulkActionsDropdownAlpine()."', '".$this->basicTable->getTableId()."', '".$this->basicTable->getPrimaryKey()."')", $topLevelAttributesArray['x-data']);
     }
-
 }
