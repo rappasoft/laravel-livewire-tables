90900d8c2c76c9d7214d6b3294ddb24aa083770a
lrljoe <lrljoe@users.noreply.github.com>
2024-09-12 00:27:55 +0000

    Fix styling

--- tests/Traits/Core/CustomAttributesTest.php
+++ tests/Traits/Core/CustomAttributesTest.php
@@ -7,40 +7,37 @@ use Rappasoft\LaravelLivewireTables\Tests\TestCase;
 
 final class CustomAttributesTest extends TestCase
 {
-
     public function test_can_get_custom_attribute_defaults_false_standard_mode(): void
     {
         $this->assertSame([
-            'default' => false, 
-            'default-colors' => true, 
-            'default-styling' => true
+            'default' => false,
+            'default-colors' => true,
+            'default-styling' => true,
         ], $this->basicTable->getCustomAttributes('bulkActionsButtonAttributes', false, true));
     }
 
     public function test_can_get_custom_attribute_defaults_false_classic_mode(): void
     {
         $this->assertSame([
-            'default-colors' => true, 
-            'default-styling' => true
+            'default-colors' => true,
+            'default-styling' => true,
         ], $this->basicTable->getCustomAttributes('bulkActionsButtonAttributes', false, false));
     }
 
     public function test_can_get_custom_attribute_defaults_true_standard_mode(): void
     {
         $this->assertSame([
-            'default' => true, 
-            'default-colors' => true, 
-            'default-styling' => true
+            'default' => true,
+            'default-colors' => true,
+            'default-styling' => true,
         ], $this->basicTable->getCustomAttributes('bulkActionsButtonAttributes', true, true));
     }
 
     public function test_can_get_custom_attribute_defaults_true_classic_mode(): void
     {
         $this->assertSame([
-            'default-colors' => true, 
-            'default-styling' => true
+            'default-colors' => true,
+            'default-styling' => true,
         ], $this->basicTable->getCustomAttributes('bulkActionsButtonAttributes', true, false));
     }
-
-
 }
