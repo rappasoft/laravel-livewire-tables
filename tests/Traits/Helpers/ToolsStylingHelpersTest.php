05bd3b17f9815753883a41d6c394fbbf409e8364
LRLJoe <joe@lowerrocklabs.com>
2024-09-29 23:58:08 +0100

    Add More Tests

--- tests/Traits/Helpers/ToolsStylingHelpersTest.php
+++ tests/Traits/Helpers/ToolsStylingHelpersTest.php
@@ -20,6 +20,15 @@ final class ToolsStylingHelpersTest extends TestCase
     {
         $this->basicTable->setToolsAttributes(['class' => 'bg-red-500', 'default-colors' => true, 'default-styling' => true]);
         $this->assertSame(['class' => 'bg-red-500', 'default-colors' => true, 'default-styling' => true], $this->basicTable->getToolsAttributesBag()->getAttributes());
+        $this->assertSame(['class' => '', 'default-colors' => true, 'default-styling' => true], $this->basicTable->getToolBarAttributesBag()->getAttributes());
+    }
+
+    public function test_can_change_tools_attributes_initial_values_no_defaults(): void
+    {
+        $this->basicTable->setToolsAttributes(['class' => 'bg-amber-500']);
+        $this->assertSame(['class' => 'bg-amber-500', 'default-colors' => false, 'default-styling' => false], $this->basicTable->getToolsAttributesBag()->getAttributes());
+        $this->assertSame(['class' => '', 'default-colors' => true, 'default-styling' => true], $this->basicTable->getToolBarAttributesBag()->getAttributes());
+
     }
 
     public function test_can_get_toolbar_attributes_initial_status(): void
@@ -36,5 +45,24 @@ final class ToolsStylingHelpersTest extends TestCase
     {
         $this->basicTable->setToolBarAttributes(['class' => 'bg-blue-500', 'default-colors' => true, 'default-styling' => true]);
         $this->assertSame(['class' => 'bg-blue-500', 'default-colors' => true, 'default-styling' => true], $this->basicTable->getToolBarAttributesBag()->getAttributes());
+        $this->assertSame(['class' => '', 'default-colors' => true, 'default-styling' => true], $this->basicTable->getToolsAttributesBag()->getAttributes());
+
     }
+
+    public function test_can_change_toolbar_attributes_initial_values_no_defaults(): void
+    {
+        $this->basicTable->setToolBarAttributes(['class' => 'bg-green-500']);
+        $this->assertSame(['class' => 'bg-green-500', 'default-colors' => false, 'default-styling' => false], $this->basicTable->getToolBarAttributesBag()->getAttributes());
+        $this->assertSame(['class' => '', 'default-colors' => true, 'default-styling' => true], $this->basicTable->getToolsAttributesBag()->getAttributes());
+    }
+
+    public function test_can_change_tools_and_toolbar_attributes_initial_values_no_defaults(): void
+    {
+        $this->basicTable->setToolsAttributes(['class' => 'bg-amber-500'])->setToolBarAttributes(['class' => 'bg-green-500']);
+        
+        $this->assertSame(['class' => 'bg-amber-500', 'default-colors' => false, 'default-styling' => false], $this->basicTable->getToolsAttributesBag()->getAttributes());
+
+        $this->assertSame(['class' => 'bg-green-500', 'default-colors' => false, 'default-styling' => false], $this->basicTable->getToolBarAttributesBag()->getAttributes());
+    }
+
 }
