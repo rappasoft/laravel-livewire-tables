e04777d9abeec7b97c72cf73655f18b0d9332854
Joe <104938042+lrljoe@users.noreply.github.com>
2024-12-24 03:40:31 +0000

    Add Get Defaults

--- tests/Unit/Traits/Styling/FilterInputDefaultStylingTest.php
+++ tests/Unit/Traits/Styling/FilterInputDefaultStylingTest.php
@@ -14,9 +14,11 @@ final class FilterInputDefaultStylingTest extends TestCase
         $this->assertFalse($this->basicTable->hasDefaultFilterInputStyling());
     }
 
-    public function test_has_filter_default_input_colors(): void
+    public function test_can_get_filter_default_input_styling(): void
     {
-        $this->assertFalse($this->basicTable->hasDefaultFilterInputColors());
+        $this->assertFalse($this->basicTable->hasDefaultFilterInputStyling());
+
+        $this->assertSame('block w-full rounded-md shadow-sm transition duration-150 ease-in-out focus:ring focus:ring-opacity-50', $this->getDefaultFilterInputStyling());
     }
 
     public function test_can_set_filter_default_input_styling(): void
@@ -28,6 +30,11 @@ final class FilterInputDefaultStylingTest extends TestCase
         $this->assertTrue($this->basicTable->hasDefaultFilterInputStyling());
     }
 
+    public function test_has_filter_default_input_colors(): void
+    {
+        $this->assertFalse($this->basicTable->hasDefaultFilterInputColors());
+    }
+
     public function test_can_set_filter_default_input_colors(): void
     {
         $this->assertFalse($this->basicTable->hasDefaultFilterInputColors());
@@ -36,4 +43,12 @@ final class FilterInputDefaultStylingTest extends TestCase
 
         $this->assertTrue($this->basicTable->hasDefaultFilterInputColors());
     }
+
+    public function test_can_get_filter_default_input_colors(): void
+    {
+        $this->assertFalse($this->basicTable->hasDefaultFilterInputColors());
+
+        $this->assertSame('border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 dark:bg-gray-800 dark:text-white dark:border-gray-600', $this->getDefaultFilterInputColors());
+    }
+
 }
