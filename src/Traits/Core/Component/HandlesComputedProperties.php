a57de60bed8a61515dc4fa3c85bf683c5f856f5b
Joe <104938042+lrljoe@users.noreply.github.com>
2025-01-08 04:07:39 +0000

    Add missing Columns Non Computed Test (#2162)
    * Add missing Columns Non Computed Test
    
    * Fix styling
    
    ---------
    
    Co-authored-by: lrljoe <lrljoe@users.noreply.github.com>

--- src/Traits/Core/Component/HandlesComputedProperties.php
+++ src/Traits/Core/Component/HandlesComputedProperties.php
@@ -6,18 +6,21 @@ trait HandlesComputedProperties
 {
     protected bool $useComputedProperties = true;
 
-    public function useComputedPropertiesEnabled(): self
+    protected function setComputedPropertiesStatus(bool $useComputedProperties): self
     {
-        $this->useComputedProperties = true;
+        $this->useComputedProperties = $useComputedProperties;
 
         return $this;
     }
 
+    public function useComputedPropertiesEnabled(): self
+    {
+        return $this->setComputedPropertiesStatus(true);
+    }
+
     public function useComputedPropertiesDisabled(): self
     {
-        $this->useComputedProperties = false;
-
-        return $this;
+        return $this->setComputedPropertiesStatus(false);
     }
 
     public function getComputedPropertiesStatus(): bool
---
+++ tests/Unit/Traits/WithColumnsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Rappasoft\LaravelLivewireTables\Tests\Unit\Traits;
+
+use Rappasoft\LaravelLivewireTables\Tests\Http\Livewire\PetsTable;
+use Rappasoft\LaravelLivewireTables\Tests\TestCase;
+
+final class WithColumnsTest extends TestCase
+{
+    public function test_rendering_with_columns_returns_columns(): void
+    {
+
+        $testTableDefault = new class extends PetsTable
+        {
+            public function configure(): void
+            {
+                parent::configure();
+                $this->useComputedPropertiesDisabled();
+
+            }
+        };
+
+        $view = view('livewire-tables::datatable');
+
+        $testTableDefault->boot();
+        $testTableDefault->mountManagesFilters();
+        $testTableDefault->bootedComponentUtilities();
+        $testTableDefault->bootedManagesFilters();
+        $testTableDefault->bootedWithColumns();
+        $testTableDefault->bootedWithColumnSelect();
+        $testTableDefault->bootedWithSecondaryHeader();
+        $testTableDefault->booted();
+        $testTableDefault->renderingWithColumns($view, $view->getData());
+        $testTableDefault->renderingWithColumnSelect($view, $view->getData());
+        $testTableDefault->renderingWithCustomisations($view, $view->getData());
+        $testTableDefault->renderingWithData($view, $view->getData());
+        $testTableDefault->renderingWithFooter($view, $view->getData());
+        $testTableDefault->renderingWithReordering($view, $view->getData());
+        $testTableDefault->renderingWithPagination($view, $view->getData());
+        $testTableDefault->render();
+        $this->assertSame(9, $view->getData()['columns']->count());
+    }
+}
