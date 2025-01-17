4f0c1c2ab3f9b41e2572c3526e77d0fa56a9db36
Hussam Alhennawi <alhennawi.hussam@gmail.com>
2025-01-17 23:46:53 +0400

    Enable setting styles for filter pills (item, reset button and reset all button) as a configuration.

--- resources/views/components/tools/filter-pills/buttons/reset-all.blade.php
+++ resources/views/components/tools/filter-pills/buttons/reset-all.blade.php
@@ -5,10 +5,16 @@
         @class([
             'focus:outline-none active:outline-none'
         ])>
-        <span @class([
-            'inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium',
-            'bg-gray-100 text-gray-800 dark:bg-gray-200 dark:text-gray-900'
-            ])>
+        <span
+            {{
+                $attributes->merge($this->getFilterPillsResetAllButtonAttributes())
+                ->class([
+                    'inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium' => $this->getFilterPillsResetAllButtonAttributes()['default-styling'],
+                    'bg-gray-100 text-gray-800 dark:bg-gray-200 dark:text-gray-900' => $this->getFilterPillsResetAllButtonAttributes()['default-colors'],
+                ])
+                ->except(['default-styling', 'default-colors'])
+            }}
+        >
             {{ __($this->getLocalisationPath.'Clear') }}
         </span>
     </button>
@@ -16,10 +22,15 @@
     <a
         href="#"
         wire:click.prevent="setFilterDefaults"
-        @class([
-            'badge badge-pill badge-light' => $isBootstrap4,
-            'badge rounded-pill bg-light text-dark text-decoration-none' => $isBootstrap5,
-        ])>
+        {{
+            $attributes->merge($this->getFilterPillsResetAllButtonAttributes())
+            ->class([
+                'badge badge-pill badge-light' => $isBootstrap4 && $this->getFilterPillsResetAllButtonAttributes()['default-styling'],
+                'badge rounded-pill bg-light text-dark text-decoration-none' => $isBootstrap5 && $this->getFilterPillsResetAllButtonAttributes()['default-styling'],
+            ])
+            ->except(['default-styling', 'default-colors'])
+        }}
+    >
         {{ __($this->getLocalisationPath.'Clear') }}
     </a>
 @endif
--- resources/views/components/tools/filter-pills/buttons/reset-filter.blade.php
+++ resources/views/components/tools/filter-pills/buttons/reset-filter.blade.php
@@ -4,10 +4,14 @@
     <button
         wire:click="resetFilter('{{ $filterKey }}')"
         type="button"
-        @class([
-            'flex-shrink-0 ml-0.5 h-4 w-4 rounded-full inline-flex items-center justify-center focus:outline-none',
-            'text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:bg-indigo-500 focus:text-white',
-        ])
+        {{
+            $attributes->merge($this->getFilterPillsResetFilterButtonAttributes())
+            ->class([
+                'flex-shrink-0 ml-0.5 h-4 w-4 rounded-full inline-flex items-center justify-center focus:outline-none' => $this->getFilterPillsResetFilterButtonAttributes()['default-styling'],
+                'text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:bg-indigo-500 focus:text-white' => $this->getFilterPillsResetFilterButtonAttributes()['default-colors'],
+            ])
+            ->except(['default-styling', 'default-colors'])
+        }}
     >
         <span class="sr-only">{{ __($this->getLocalisationPath.'Remove filter option') }}</span>
         <x-heroicon-m-x-mark class="h-full" />
@@ -16,9 +20,13 @@
     <a
         href="#"
         wire:click="resetFilter('{{ $filterKey }}')"
-        @class([
-            'text-white ml-2' => ($isBootstrap),
-        ])
+        {{
+            $attributes->merge($this->getFilterPillsResetFilterButtonAttributes())
+            ->class([
+                'text-white ml-2' => $isBootstrap && $this->getFilterPillsResetFilterButtonAttributes()['default-styling']
+            ])
+            ->except(['default-styling', 'default-colors'])
+        }}
     >
         <span @class([
             'sr-only' => $isBootstrap4,
