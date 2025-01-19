74f6a5fbac6ffa8e5285c566054ec48d6bcff275
Joe <104938042+lrljoe@users.noreply.github.com>
2025-01-19 12:03:29 +0000

    Adjust configurable areas test

--- tests/Unit/Traits/Helpers/ComponentHelpersTest.php
+++ tests/Unit/Traits/Helpers/ComponentHelpersTest.php
@@ -191,6 +191,7 @@ final class ComponentHelpersTest extends TestCase
             'toolbar-right-end' => null,
             'before-toolbar' => null,
             'after-toolbar' => null,
+            'after-tools' => null,
             'before-pagination' => null,
             'after-pagination' => null,
         ], $this->basicTable->getConfigurableAreas());
