599cc33c7446369197cb2f2fa3e28f65311d6797
LRLJoe <joe@lowerrocklabs.com>
2025-01-31 09:15:11 +0000

    Minor dochint fixes

--- src/Traits/Filters/HandlesPillsData.php
+++ src/Traits/Filters/HandlesPillsData.php
@@ -9,25 +9,22 @@ trait HandlesPillsData
         $filters = [];
 
         foreach ($this->getAppliedFiltersWithValuesForPills() as $filterSelectName => $value) {
-            if (! is_null($filterSelectName)) {
-                if (! is_null($filter = $this->getFilterByKey($filterSelectName))) {
-                    if ($filter->isEmpty($value)) {
-                        continue;
-                    }
-                    $filterPillValue = $filter->getFilterPillValue($value);
-                    $filterPillTitle = $filter->getFilterPillTitle();
-
-                    $filters[$filter->getKey()] = [
-                        'filter' => $filter,
-                        'isAnExternalLivewireFilter' => (method_exists($filter, 'isAnExternalLivewireFilter') && $filter->isAnExternalLivewireFilter()),
-                        'filterSelectName' => $filterSelectName,
-                        'filterPillTitle' => $filterPillTitle,
-                        'filterPillValue' => $filterPillValue,
-                        'separator' => method_exists($filter, 'getPillsSeparator') ? $filter->getPillsSeparator() : ', ',
-                    ];
+            if (! is_null($filter = $this->getFilterByKey($filterSelectName))) {
+                if ($filter->isEmpty($value)) {
+                    continue;
                 }
-            }
+                $filterPillValue = $filter->getFilterPillValue($value);
+                $filterPillTitle = $filter->getFilterPillTitle();
 
+                $filters[$filter->getKey()] = [
+                    'filter' => $filter,
+                    'isAnExternalLivewireFilter' => (method_exists($filter, 'isAnExternalLivewireFilter') && $filter->isAnExternalLivewireFilter()),
+                    'filterSelectName' => $filterSelectName,
+                    'filterPillTitle' => $filterPillTitle,
+                    'filterPillValue' => $filterPillValue,
+                    'separator' => method_exists($filter, 'getPillsSeparator') ? $filter->getPillsSeparator() : ', ',
+                ];
+            }
         }
 
         return $filters;
