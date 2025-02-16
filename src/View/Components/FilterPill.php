35cd35a87fb00edc4a314d209d2fe58dba3d7328
Joe <104938042+lrljoe@users.noreply.github.com>
2025-02-16 23:55:07 +0000

    Tweak type

--- src/View/Components/FilterPill.php
+++ src/View/Components/FilterPill.php
@@ -11,7 +11,7 @@ class FilterPill extends Component
 
     public function __construct(public string $filterKey, public FilterPillData $filterPillData)
     {
-        $this->shouldWatch = $this->filterPillData->shouldWatchForEvents();
+        $this->shouldWatch = (bool) $this->filterPillData->shouldWatchForEvents();
     }
 
     public function render(): null|string|\Illuminate\Support\HtmlString|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
