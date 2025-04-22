b8a86f88ffd3bbb02f7f72124a1f69a32b500c9c
Joe McElwee <joe@lowerrocklabs.com>
2025-04-22 03:39:43 +0100

    Additional Tests for LivewireComponentColumn

--- tests/TestServiceProvider.php
+++ tests/TestServiceProvider.php
@@ -11,6 +11,9 @@ class TestServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Blade::component('test-component', TestComponent::class);
+
+        \Livewire\Livewire::component('test-livewire-column-component', \Rappasoft\LaravelLivewireTables\Tests\Http\Livewire\TestLivewireColumnComponent::class);
+
         $this->loadViewsFrom(__DIR__.'/views', 'livewire-tables-test');
 
     }
