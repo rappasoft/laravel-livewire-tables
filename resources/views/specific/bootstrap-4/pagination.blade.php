d9b78959716287353b63ca8e0597787eb8028a13
daniel-skopek <daniel.skopek1@gmail.com>
2025-04-26 23:52:23 +0200

    Improved pagination UX

--- resources/views/specific/bootstrap-4/pagination.blade.php
+++ resources/views/specific/bootstrap-4/pagination.blade.php
@@ -3,7 +3,7 @@
         @php(isset($this->numberOfPaginatorsRendered[$paginator->getPageName()]) ? $this->numberOfPaginatorsRendered[$paginator->getPageName()]++ : $this->numberOfPaginatorsRendered[$paginator->getPageName()] = 1)
 
         <nav>
-            <ul class="pagination">
+            <ul class="pagination d-flex flex-wrap justify-content-start">
                 {{-- Previous Page Link --}}
                 @if ($paginator->onFirstPage())
                     <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
