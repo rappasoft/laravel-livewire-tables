f13fef77c405359dd820333cfbc97b5653ba9324
lrljoe <104938042+lrljoe@users.noreply.github.com>
2025-05-02 23:26:48 +0000

    Fix styling

--- tests/Unit/Views/Columns/ArrayColumnTest.php
+++ tests/Unit/Views/Columns/ArrayColumnTest.php
@@ -80,14 +80,14 @@ final class ArrayColumnTest extends ColumnTestCase
         $this->assertSame('Unknown', self::$columnInstance->getEmptyValue());
 
     }
-    
+
     public function test_can_use_wrapper(): void
     {
         self::$columnInstance
             ->data(fn ($value, $row) => ($row->pets))
             ->outputFormat(fn ($index, $value) => '<a href="'.$value->id.'">'.$value->name.'</a>')
             ->wrapperStart('<div class="start-of-wrapper">')
-            ->wrapperEnd("</div>");
+            ->wrapperEnd('</div>');
 
         $contents = self::$columnInstance->getContents(Veterinary::find(1));
         $this->assertSame('<div class="start-of-wrapper"><a href="1">Cartman</a><a href="2">Tux</a></div>', $contents->toHtml());
@@ -99,7 +99,7 @@ final class ArrayColumnTest extends ColumnTestCase
             ->data(fn ($value, $row) => ($row->pets))
             ->outputFormat(fn ($index, $value) => '<li><a href="'.$value->id.'">'.$value->name.'</a></li>')
             ->wrapperStart('<ul class="start-of-wrapper">')
-            ->wrapperEnd("</ul>");
+            ->wrapperEnd('</ul>');
 
         $contents = self::$columnInstance->getContents(Veterinary::find(1));
         $this->assertSame('<ul class="start-of-wrapper"><li><a href="1">Cartman</a></li><li><a href="2">Tux</a></li></ul>', $contents->toHtml());
